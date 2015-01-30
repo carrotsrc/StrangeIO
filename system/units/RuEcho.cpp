fb22ab3a14be0c4c2511b7c43819906f25bfbd22
Charlie FG <charlie@carrotsrc.org>
2015-01-30 08:35:57 +0000

    Started implementing delay functions

--- system/units/RuEcho.cpp
+++ system/units/RuEcho.cpp
@@ -5,26 +5,86 @@ RuEcho::RuEcho()
 : RackUnit() {
 	addJack("audio", JACK_SEQ);
 	addPlug("audio_out");
+	mDelay = 50;
+	sampleRate = 44100;
 	workState = IDLE;
 }
 
 FeedState RuEcho::feed(RackoonIO::Jack *jack) {
+	short *period;
+	Jack *out = getPlug("audio_out")->jack;
 	if(workState == BYPASS) {
-		short *tbuf;
-		if(jack->flush(&tbuf) == FEED_OK) {
+		if(jack->flush(&period) == FEED_OK) {
 			Jack *out = getPlug("audio_out")->jack;
 			out->frames = jack->frames;
-			return out->feed(tbuf);
+			return out->feed(period);
+		}
+		return FEED_OK;
+	}
+
+	if(workState == PRIMING) {
+		if(jack->flush(&period) == FEED_OK) {
+			if(fLevel + jack->frames > bufSize) {
+				workState = RUNNING;
+				cout << "Switched to RUNNING" << endl;
+				out->frames = jack->frames;
+				fPosition = frameBuffer;
+				fLevel = 0;
+				return out->feed(add(period, out->frames));
+			} else {
+				memcpy(fPosition, period, (jack->frames*sizeof(short)));
+				fPosition += jack->frames;
+				fLevel += jack->frames;
+				out->frames = jack->frames;
+				return out->feed(period);
+			}
 		}
 	}
+
+	if(workState == RUNNING) {
+		if(jack->flush(&period) == FEED_OK) {
+			out->frames = jack->frames;
+			return out->feed(period);
+		}
+	}
+
 }
 
-void RuEcho::setConfig(string,string) {
+short *RuEcho::add(short *period, int size) {
+	short *addedPeriod = (short*)malloc(sizeof(short)*size);
+	if(dLevel + size > bufSize) {
+		fLevel = dLevel = 0;
+		fPosition = fDelay = frameBuffer;
+	}
 
+	for(int i = 0; i < size; i++)
+		addedPeriod[i] = period[i] + fDelay[i];
+
+	fDelay += size;
+	dLevel += size;
+	memcpy(fPosition, period, sizeof(short)*size);
+
+	fPosition += size;
+	fLevel += size;
+
+	free(period);
+
+	return addedPeriod;
+}
+
+void RuEcho::setConfig(string config, string value) {
+	if(config == "delay")
+		mDelay = atoi(value.c_str());
 }
 
 RackState RuEcho::init() {
-	workState = BYPASS;
+	workState = PRIMING;
+	bufSize = ((sampleRate/1000)*mDelay);
+	frameBuffer = (short*) malloc(sizeof(short) * (bufSize));
+	fPosition = frameBuffer;
+	fDelay = frameBuffer;
+	dLevel = fLevel = 0;
+
 	cout << "RuEcho: Initialised" << endl;
 }
 
--- system/units/RuEcho.h
+++ system/units/RuEcho.h
@@ -6,12 +6,15 @@ class RuEcho : public RackoonIO::RackUnit
 	enum WorkState {
 		IDLE,
 		READY,
+		PRIMING,
 		RUNNING,
 		BYPASS
 	};
 
 	WorkState workState;
-
+	unsigned int mDelay, sampleRate, bufSize;
+	short *frameBuffer, *fDelay, *fPosition, fLevel, dLevel;
+	short *add(short*, int);
 public:
 	RuEcho();
 	RackoonIO::FeedState feed(RackoonIO::Jack*);

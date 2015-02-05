2518fbfb33b9d950b7032dd59a674537fcb7bb80
Charlie FG <charlie@carrotsrc.org>
2015-02-05 11:42:16 +0000

    removed unworkable code

--- system/units/RuPitchBender.cpp
+++ system/units/RuPitchBender.cpp
@@ -15,47 +15,12 @@ RuPitchBender::RuPitchBender()
 
 FeedState RuPitchBender::feed(Jack *jack) {
 	Jack *out = getPlug("audio_out")->jack;
+	int frames = jack->frames;
 
 	if(bentPeriod != nullptr && out->feed(bentPeriod) == FEED_WAIT)
 		return FEED_WAIT;
 
-	bentPeriod = nullptr;
 
-	short *period;
-	int k;
-	jack->flush(&period);
-	if(oldBend != bend) {
-		inc = (((float)jack->frames/100)*bend)<<1;
-		step = jack->frames/inc;
-		nsize = jack->frames+inc;
-		oldBend = bend;
-	}
-
-	k = 0;
-	bentPeriod = (short*)malloc(sizeof(short)*nsize);
-	for(int i = 0; i < jack->frames;) {
-		if(i > 0 && i%step == 0) {
-			bentPeriod[k++] = (period[i]>>1);
-			if(period[i] < period[i]+1) // up hill
-				bentPeriod[k++] = period[i] + ((period[i+1] - period[i])>>1);
-			else
-			if(period[i] > period[i]+1) // down hill
-				bentPeriod[k++] = period[i] - (period[i] - period[i+1]>>1);
-			else
-				bentPeriod[k++] = period[i];
-
-
-			continue;
-		}
-		bentPeriod[k++] = (period[i]>>1);
-	}
-
-	free(period);
-
-	out->frames = nsize;
-	if(out->feed(bentPeriod) == FEED_WAIT)
-		return FEED_OK;
-	bentPeriod = nullptr;
 	return FEED_OK;
 }
 

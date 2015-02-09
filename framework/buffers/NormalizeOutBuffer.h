925a4dfd763dd7f06b91c4596057ad93e05c63b9
Charlie FG <charlie@carrotsrc.org>
2015-02-09 22:51:31 +0000

    Implementing normalized size buffer in library

--- framework/buffers/NormalizeOutBuffer.h
+++ framework/buffers/NormalizeOutBuffer.h
@@ -1,17 +1,17 @@
 #ifndef NORMALIZEOUTBUFFER_H
 #define NORMALIZEOUTBUFFER_H
+#include "common.h"
+
+namespace RackoonIO {
 
 template<typename T>
 class NormalizeOutBuffer {
 private:
 
-	T *buffer, *remainder, *whead, *rhead;
+	T *buffer, *remainder;
 
-	int capacity;
-	int normal;
-	int threshold;
-	int loadline;
-	int dtsize;
+	int capacity, normal, 
+	    loadLine, remLine, dtsize;
 
 public:
 	NormalizeOutBuffer(int);
@@ -19,9 +19,6 @@ public:
 
 	void setNormalOut(int);
 	int getNormalOut();
-
-	void setThreshold(int);
-	int getThreshold();
 
 	int getLoadline();
 	int getCapacity();
@@ -32,4 +29,79 @@ public:
 	T *read();
 	void write(T*,int);
 };
+
+/*
+ * Template definitions
+ */
+
+
+template<typename T>
+NormalizeOutBuffer<T>::NormalizeOutBuffer(int size) {
+	buffer = (T*) malloc(sizeof(T)*size);
+	normal = 0;
+	capacity = size;
+}
+
+template<typename T>
+NormalizeOutBuffer<T>::NormalizeOutBuffer(int size, int outNormal) {
+	buffer = (T*) malloc(sizeof(T)*size);
+	normal = outNormal;
+	capacity = size;
+	dtsize = sizeof(T);
+}
+
+template<typename T>
+void NormalizeOutBuffer<T>::setNormalOut(int outNormal) {
+	normal = outNormal;
+	dtsize = sizeof(T);
+}
+
+template<typename T>
+int NormalizeOutBuffer<T>::getNormalOut() {
+	return normal;
+}
+
+template<typename T>
+int NormalizeOutBuffer<T>::getLoadline() {
+	return loadLine;
+}
+
+template<typename T>
+int NormalizeOutBuffer<T>::getCapacity() {
+	return capacity;
+}
+
+template<typename T>
+bool NormalizeOutBuffer<T>::writeReady(int size) {
+}
+
+template<typename T>
+bool NormalizeOutBuffer<T>::readReady() {
+	if(buffer == nullptr)
+		return false;
+	return true;
+}
+
+template<typename T>
+T* NormalizeOutBuffer<T>::read() {
+	return buffer;
+}
+
+template<typename T>
+void NormalizeOutBuffer<T>::write(T *data, int size) {
+	if((size + remLine) >= normal) {
+		int diff = normal-remLine, r = remLine;
+
+		for(int i = 0; i < diff; i++)
+			*(buffer+remLine+i) = *(data+i);
+
+		remLine = size+remLine-normal;
+		memcpy(remainder, data+(diff), dtsize*remLine);
+	} else {
+		buffer = nullptr;
+		memcpy(remainder+remLine, data, dtsize*size);
+	}
+}
+
+} // RackoonIO
 #endif

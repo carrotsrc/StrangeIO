d641a775bf6e9be58fcd7ad852bbd917e93bf4d5
Charlie FG <charlie@carrotsrc.org>
2015-02-26 19:34:07 +0000

    Commented DelayBuffer

--- framework/buffers/DelayBuffer.h
+++ framework/buffers/DelayBuffer.h
@@ -22,14 +22,35 @@
 namespace RackoonIO {
 
 namespace Buffers {
+/** Buffer to act as a general delay line
+ *
+ * This buffer implements a general purpose delay line. It's
+ * main usage is passing it periods of samples until its load
+ * reaches it's capacity. When the buffer is flushed, it flushes
+ * all the samples at once.
+ *
+ * General usage:
+ *
+ * Keep supplying it periods of samples until its state changes
+ * from OK to WAIT, at which point is at capacity
+ *
+ * When you need the samples, flush it to get a pointer to all the samples
+ */
+
+/** @todo
+ * Once samples are set to a standard data type (floating point) then
+ * this buffer can be given a pointer to a cache object to flush into
+ * directly instead of two memcpys to get it into cache
+ */
 
 template<typename T>
 class DelayBuffer
 {
 public:
+	/// The various states the buffer will take on
 	enum State {
-		OK,
-		WAIT
+		OK, ///< The buffer can be supplied more data
+		WAIT ///< The buffer is full and can't take any more
 	};
 	typedef State E;
 	DelayBuffer(int);
@@ -44,6 +65,10 @@ private:
 
 // Template Defintions
 
+/** Initialise the buffer size
+ *
+ * The interal buffer is allocated with the specified size
+ */
 template<typename T>
 DelayBuffer<T>::DelayBuffer(int size) {
 	bSize = size;
@@ -51,11 +76,24 @@ DelayBuffer<T>::DelayBuffer(int size) {
 	load = 0;
 }
 
+/** Get the current sample load
+ *
+ * @return The number of samples in the buffer
+ */
 template<typename T>
 int DelayBuffer<T>::getLoad() {
 	return load;
 }
 
+/** Supply a sample period to the buffer
+ *
+ * Supply a period of pSize samples. The method
+ * will return the state of the buffer
+ *
+ * @param period A pointer to the period of samples
+ * @param pSize The number of samples in period
+ * @return OK if frames were copied; WAIT if the buffer's load is at full capacity
+ */
 template<typename T>
 typename DelayBuffer<T>::State
 DelayBuffer<T>::supply(const T *period, int pSize) {
@@ -67,6 +105,13 @@ DelayBuffer<T>::supply(const T *period, int pSize) {
 	return OK;
 }
 
+/** Flush all the samples out of the buffer
+ *
+ * This method will return a pointer to the samples
+ * and reset the load to zero.
+ *
+ * @return A const pointer to the samples
+ */
 template<typename T>
 const T* DelayBuffer<T>::flush() {
 	load = 0;

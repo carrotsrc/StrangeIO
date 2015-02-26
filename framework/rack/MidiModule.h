ad301e2d1f53b5748040f326af0701420ac99daa
Charlie FG <charlie@carrotsrc.org>
2015-02-26 23:26:09 +0000

    Commented MidiModule

--- framework/rack/MidiModule.h
+++ framework/rack/MidiModule.h
@@ -18,27 +18,76 @@
 #include "common.h"
 namespace RackoonIO {
 
+
+/** Basic representation of a MIDI control code */
 typedef struct {
-	char f;
-	char n;
-	char v;
+	char f; ///< function code
+	char n; ///< note code
+	char v; ///< Velocity of note
 } MidiCode;
 
-class MidiModule {
-	snd_rawmidi_t *inMidi;
-	std::string portName;
-	std::string alias;
-	char buffer[3];
 
+/** A representation and handle of a MIDI device
+ *
+ * When a device is set in the configuration, a MidiModule object
+ * is created as the handle and interface for the device.
+ *
+ * When an object is configured as bound to a code, the callback is
+ * passed to the respective MIDI module.
+ *
+ * The module then deals with sending the velocity of the MIDI signal 
+ * to the RackUnit that is bound to that code.
+ *
+ * The module gets any messages from the port handle on each cycle
+ */
+class MidiModule {
+	snd_rawmidi_t *inMidi; ///< input port handle
+	std::string portName; ///< Unique ID of port handle (e.g. hw 1,0,0 )
+	std::string alias; ///< Device alias in the system
+	char buffer[3]; ///< buffer for a MIDI control message
+
+	/** An map of exported callbacks bound to MIDI controller */
 	std::map<int, std::function<void(int)> > bindings;
 public:
-	MidiModule(string, string);
+	/** Setup the device
+	 *
+	 * @param port The unique ID of the port
+	 * @param alias The system alias of the device
+	 */
+	MidiModule(string port, string alias);
+
+	/** Initialiases the device by opening the port
+	 *
+	 * @return true on success; false on error
+	 */
 	bool init();
+
+	/** Cycle the module to retrieve any queue MIDI messages and call any bindings
+	 */
 	void cycle();
+
+	/** Retrieve a single MIDI message if it is queued in the driver buffer
+	 *
+	 * @return The a filled out MidiCode if one is waiting; otherwise a zeroed MidiCode structure
+	 */
 	MidiCode flush();
 
+	/** get the unique port ID
+	 *
+	 * @return The string port name assigned to the module
+	 */
 	std::string getPort();
+
+	/** get the alias assigned to the module
+	 *
+	 * @return The string alias assigned to the module
+	 */
 	std::string getAlias();
+
+	/** Add a binding to a controller
+	 *
+	 * Use this method to bind a callback to a specific MIDI controller
+	 */
 	void addBinding(double, std::function<void(int)>);
 };
 

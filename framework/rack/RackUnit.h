4410d239b9b1fd860b3c4c0c805c1711f6708131
Charlie FG <charlie@carrotsrc.org>
2015-02-27 12:01:06 +0000

    Commenting RackUnit

--- framework/rack/RackUnit.h
+++ framework/rack/RackUnit.h
@@ -35,40 +35,154 @@ enum UnitState {
 	UNIT_OFF
 };
 
+
+/** The base class for a processing unit in the Rack
+ *
+ * This is where all the processing action happens. A RackUnit
+ * is an abstraction of a unit of audio processing hardware on
+ * a rack.
+ *
+ * Each RackUnit is placed in a daisychain and performs a specific 
+ * task like loading PCM samples from a file, applying a single
+ * effect, mixing.
+ *
+ * It can be assumed that protected methods are client facing API
+ * for the derived classes.
+ *
+ * When the RackUnit receives samples through it's feed method, it can either
+ * accept the samples by returning FEED_OK, or tell the upstream unit to
+ * hold on until the current processing has finished or is ready to accept
+ * more data by returning FEED_WAIT.
+ *
+ * Once the data is accepted (by flushing the jack), the RackUnit can either 
+ * outsource the task to a worker thread or perform the processing in the feed 
+ * method.
+ *
+ * If the downstream can't accept the processed data, the unit must store
+ * the data and wait for another opportunity, either from an AC signal or
+ * another upstream feed. The state machine can be a bit of a headache to 
+ * work out.
+ *
+ * The RackUnit can use the buffers provided or build its own.
+ *
+ * @todo This also is soon to become a demi-god and functionality needs breaking up
+ */
 class RackUnit
 {
-	vector<Jack*> jackArray;
-	vector<Plug*> plugArray;
+	vector<Jack*> jackArray; ///< A vector of Jack objects
+	vector<Plug*> plugArray; ///< A vector Plugs objects
 
-	std::string name;
-	std::string rutype;
+	std::string name; ///< The unique name of the RackUnit
+	std::string rutype; ///< The string type name (e.g. the class name)
 
-	RackQueue *rackQueue;
-	GenericEventMessageFactory *messageFactory;
-	EventLoop *eventLoop;
-	CacheHandler *cacheHandler;
+	RackQueue *rackQueue; ///< A pointer to the rackQueue
+	GenericEventMessageFactory *messageFactory; ///< A pointer to the client supplied message factory
+	EventLoop *eventLoop; ///< A pointer to the event loop
+	CacheHandler *cacheHandler; ///< A pointer to the cache allocation handler
 
-	std::map<string, std::function<void(int)> > midiExport;
+	std::map<string, std::function<void(int)> > midiExport; ///< A map of exported methods to bind to MIDI
 
 
 protected:
-	RackChain *chain;
-	UnitState unitState;
+	RackChain *chain; ///< Pointer to the whole daisychain
+	UnitState unitState; ///< Current system state of the unit
 
-	void addJack(string, ConnectorType);
-	void addPlug(string);
+	/** Add a jack to the unit
+	 *
+	 * This is used by derived classes to describe the array of jacks
+	 * to be used for connections coming from upstream
+	 * 
+	 * @note
+	 * The ConnectorType is *almost* deprecated unless a real use is
+	 * found
+	 *
+	 * @param name The name given to the jack
+	 * @param type The type of jack it is
+	 */
+	void addJack(string name, ConnectorType type);
 
-	void feedOut(string, short*);
+	/** Add a plug to the unit
+	 *
+	 * This is used by derived classes to describe the array of plugs 
+	 * that can be used to connect downstream
+	 * 
+	 * @param name The name given to the plug
+	 */
+	void addPlug(string name);
 
-	void outsource(std::function<void()>);
+	void feedOut(string, short*); ///< @deprecated Unless use is found
+
+	/** Outsource a task to the thread pool
+	 *
+	 * Call this method when a task is required to be run on a separate
+	 * thread.
+	 *
+	 * This means derived classes don't have direct access to the queue
+	 * object
+	 *
+	 * @param task The task method to outsource to the thread pool
+	 */
+	void outsource(std::function<void()> task);
+
+	/** Export a method for MIDI binding
+	 *
+	 * Call this method to export a particular method in the derived
+	 * class binding to a MIDI controller. The method will act as the 
+	 * callback for when the controller code is received
+	 *
+	 * @param action The action name that can be configured for binding
+	 * @param method The method callback to perform the action
+	 */
 	void midiExportMethod(string, std::function<void(int)> );
 
-	void addEventListener(EventType, std::function<void(shared_ptr<EventMessage>)>);
-	void addEvent(std::unique_ptr<EventMessage>);
-	unique_ptr<EventMessage> createMessage(EventType);
+	/** Add an event listener to be notified of a particular event
+	 *
+	 * @param eventType The event type for which to be notified
+	 * @param callback The callback method in which to recieve the notification
+	 */
+	void addEventListener(EventType eventType, std::function<void(shared_ptr<EventMessage>)> callback);
 
-	short *cacheAlloc(int);
-	void cacheFree(short*);
+	/** Add an event to the event loop
+	 *
+	 * Call this method when an event has occurred within the system
+	 * 
+	 * @param msg The event message to be distributed
+	 */
+	void addEvent(std::unique_ptr<EventMessage> msg);
+
+	/** Create a new message based on an event
+	 *
+	 * This will contruct an event message from the supplied factory.
+	 *
+	 * The message will automatically go through a generalised upcasting
+	 * to EventMessage so it can be distributed throughout the event
+	 * layer.
+	 *
+	 * @param eventType the event type for the message
+	 * @return A unique_ptr to the instantiated message, upcast to EventMessage
+	 */
+	unique_ptr<EventMessage> createMessage(EventType eventType);
+
+	/** Allocate a new block from the cache
+	 *
+	 * Instead of doing direct memory allocation - RackUnit objects are
+	 * encouraged to use whatever cache allocation handler is supplied.
+	 *
+	 * @param num The number of blocks to allocate
+	 * @return A pointer to the allocated cache block
+	 */
+	short *cacheAlloc(int num);
+
+	/** Free a block that has been allocated from the cache
+	 *
+	 * Instead of doing direct memory allocation - RackUnit objects are
+	 * encouraged to use whatever cache allocation handler is supplied.
+	 *
+	 * This method will allow the block to be reused in the system
+	 *
+	 * @param mem The pointer to the cache block
+	 */
+	void cacheFree(short *mem);
 public:
 	RackUnit(string);
 

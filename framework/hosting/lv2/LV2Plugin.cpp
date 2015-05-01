04a226865b093607309258a8b32da00b96089558
Charlie FG <cfg@zunautica.org>
2015-05-01 19:53:32 +0100

    Implement interface methods for plugin

--- framework/hosting/lv2/LV2Plugin.cpp
+++ framework/hosting/lv2/LV2Plugin.cpp
@@ -1,4 +1,23 @@
 #include "LV2Plugin.h"
-
 using namespace RackoonIO::Hosting;
 
+
+LV2Plugin::LV2Plugin(const LilvPlugin* p) {
+	plugin = p;
+}
+
+const LilvPlugin* LV2Plugin::getPlugin() {
+	return plugin;
+}
+
+std::string LV2Plugin::getName() {
+	return std::string(lilv_node_as_string(lilv_plugin_get_name(plugin)));
+}
+
+uint32_t LV2Plugin::getNumPorts() {
+	return uint32_t(lilv_plugin_get_num_ports(plugin));
+}
+
+std::string LV2Plugin::getUri() {
+	return std::string(lilv_node_as_string(lilv_plugin_get_uri(plugin)));
+}
--- framework/hosting/lv2/LV2Plugin.h
+++ framework/hosting/lv2/LV2Plugin.h
@@ -1,11 +1,21 @@
 #ifndef LV2PLUGIN_H
 #define LV2PLUGIN_H
-
+#include <lilv/lilv.h>
+#include <cstdint>
+#include "framework/common.h"
 namespace RackoonIO {
 namespace Hosting {
 
 class LV2Plugin {
+public:
+	LV2Plugin(const LilvPlugin*);
+	const LilvPlugin* getPlugin();
 
+	std::string getName();
+	uint32_t getNumPorts();
+	std::string getUri();
+private:
+	const LilvPlugin* plugin;
 };
 
 

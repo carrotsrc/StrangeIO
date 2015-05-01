525f160e3d2d626f1d2cd979cec6ff1d1e6cec90
Charlie FG <cfg@zunautica.org>
2015-05-01 14:53:37 +0100

    Add tests for lv2 platform

---
+++ test/HostingLv2.cpp
@@ -0,0 +1,12 @@
+#include "framework/common.h"
+#include "framework/hosting/lv2/LV2Platform.h"
+using namespace std;
+
+int main( void ) {
+	RackoonIO::Hosting::LV2Platform platform;
+	string uri = "http://plugin.org.uk/swh-plugins/amp";
+	auto plugin = platform.getPlugin(uri);
+	if(plugin == nullptr) {
+		cerr << "Failed to load plugin " << uri << endl;
+	}
+}

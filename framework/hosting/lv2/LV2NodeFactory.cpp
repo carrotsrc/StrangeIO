f4d6316d8da8a3dbe50f91f39c98449e7b9021bd
Charlie FG <cfg@zunautica.org>
2015-05-02 18:06:48 +0100

    Make node factory methods const

--- framework/hosting/lv2/LV2NodeFactory.cpp
+++ framework/hosting/lv2/LV2NodeFactory.cpp
@@ -6,31 +6,31 @@ LV2NodeFactory::LV2NodeFactory(LilvWorld* w) {
 	world = w;
 }
 
-std::unique_ptr<LV2Node> LV2NodeFactory::newUri(std::string uri) {
+std::unique_ptr<LV2Node> LV2NodeFactory::newUri(std::string uri) const {
 	auto n = lilv_new_uri(world, uri.c_str());
 	return pack(n);
 }
 
-std::unique_ptr<LV2Node> LV2NodeFactory::newString(std::string str) {
+std::unique_ptr<LV2Node> LV2NodeFactory::newString(std::string str) const {
 	auto n = lilv_new_string(world, str.c_str());
 	return pack(n);
 }
 
-std::unique_ptr<LV2Node> LV2NodeFactory::newInt(int32_t val) {
+std::unique_ptr<LV2Node> LV2NodeFactory::newInt(int32_t val) const {
 	auto n = lilv_new_int(world, val);
 	return pack(n);
 }
 
-std::unique_ptr<LV2Node> LV2NodeFactory::newFloat(float val) {
+std::unique_ptr<LV2Node> LV2NodeFactory::newFloat(float val) const {
 	auto n = lilv_new_float(world, val);
 	return pack(n);
 }
 
-std::unique_ptr<LV2Node> LV2NodeFactory::newBool(bool val) {
+std::unique_ptr<LV2Node> LV2NodeFactory::newBool(bool val) const {
 	auto n = lilv_new_bool(world, val);
 	return pack(n);
 }
 
-std::unique_ptr<LV2Node> LV2NodeFactory::pack(LilvNode* n) {
+std::unique_ptr<LV2Node> LV2NodeFactory::pack(LilvNode* n) const {
 	return std::unique_ptr<LV2Node>(new LV2Node(n));
 }
--- framework/hosting/lv2/LV2NodeFactory.h
+++ framework/hosting/lv2/LV2NodeFactory.h
@@ -11,13 +11,13 @@ namespace Hosting {
 class LV2NodeFactory {
 public:
 	LV2NodeFactory(LilvWorld*);
-	std::unique_ptr<LV2Node> newUri(std::string);
-	std::unique_ptr<LV2Node> newString(std::string);
-	std::unique_ptr<LV2Node> newInt(int32_t);
-	std::unique_ptr<LV2Node> newFloat(float);
-	std::unique_ptr<LV2Node> newBool(bool);
+	std::unique_ptr<LV2Node> newUri(std::string) const;
+	std::unique_ptr<LV2Node> newString(std::string) const;
+	std::unique_ptr<LV2Node> newInt(int32_t) const;
+	std::unique_ptr<LV2Node> newFloat(float) const;
+	std::unique_ptr<LV2Node> newBool(bool) const;
 
-	std::unique_ptr<LV2Node> pack(LilvNode*);
+	std::unique_ptr<LV2Node> pack(LilvNode*) const;
 private:
 	LilvWorld* world;
 };

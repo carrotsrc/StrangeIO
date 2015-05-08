8e48f097c3382e0feeb67bf72b375062afc9abaa
Charlie FG <cfg@zunautica.org>
2015-05-08 13:32:25 +0100

    Add node casting to plugin

--- framework/hosting/lv2/LV2Plugin.cpp
+++ framework/hosting/lv2/LV2Plugin.cpp
@@ -32,13 +32,13 @@ void LV2Plugin::profilePorts() {
 	for(uint32_t i = 0; i < n; i++) {
 		auto port = lilv_plugin_get_port_by_index(plugin, i);
 		
-		//auto pname = LV2Node::as<NodeString>(lilv_port_get_name(plugin, port));
+		auto pname = LV2Node::asString(lilv_port_get_name(plugin, port));
 
 		if(lilv_port_is_a(plugin, port, **nodeIn)) {
-			//input.insert(std::pair<std::string, const LilvPort *>(std::string(pname), port));
+			input.insert(std::pair<std::string, const LilvPort *>(std::string(pname), port));
 		} else
 		if(lilv_port_is_a(plugin, port, **nodeOut)) {
-			//output.insert(std::pair<std::string, const LilvPort *>(std::string(pname), port));
+			output.insert(std::pair<std::string, const LilvPort *>(std::string(pname), port));
 		}
 	}
 }

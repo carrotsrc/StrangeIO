aeaad815efee4795f2c2a0fd58891b15c82a64f1
Charlie FG <cfg@zunautica.org>
2015-05-11 22:13:29 +0100

    Add testing

--- test/HostingLv2.cpp
+++ test/HostingLv2.cpp
@@ -1,4 +1,5 @@
 #include <typeinfo>
+#include <math.h>
 #include "framework/common.h"
 #include "framework/hosting/lv2/LV2Platform.h"
 using namespace std;
@@ -15,6 +16,27 @@ int main( void ) {
 	if(!plugin->instantiate()) {
 		cerr << "Failed to instantiate " << uri << endl;
 		exit(EXIT_FAILURE);
+	}
+	plugin->init();
+	auto ts = 1.0f/512.0f;
+	auto in = (float*) malloc(sizeof(float)*512);
+	auto out = (float*) malloc(sizeof(float)*512);
+	auto gain = 2.0f;
+
+	auto ports = plugin->getPortsOfType(RackoonIO::Hosting::LV2Port::Input);
+	std::vector<const RackoonIO::Hosting::LV2Port*>::const_iterator it;
+
+	for(int i = 0; i < 512; i++) {
+		in[i] = sin(2 * M_PI * ts * i);
+	}
+
+	plugin->connectPort("Input", (void*)in);
+	plugin->connectPort("Amps gain (dB)", &gain);
+	plugin->connectPort("Output", (void*)out);
+	plugin->run(512);
+
+	for(int i = 0; i < 512; i++) {
+		cout << in[i] << "\t" << out[i] << endl;
 	}
 
 	cout << "Successful" << endl;

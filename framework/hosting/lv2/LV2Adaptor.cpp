30160ae41e1eb000eca2f0903dedf351ffa50c0a
Charlie FG <cfg@zunautica.org>
2015-05-11 22:39:25 +0100

    Bulk out adaptor

--- framework/hosting/lv2/LV2Adaptor.cpp
+++ framework/hosting/lv2/LV2Adaptor.cpp
@@ -2,4 +2,25 @@
 
 using namespace RackoonIO::Hosting;
 
+LV2Adaptor::LV2Adaptor() {
+
+}
+
+void LV2Adaptor::setPlatform(const LV2Platform* platform) {
+	mPlatform = platform;
+}
+
+void LV2Adaptor::loadPlugin(std::string uri) {
+	mPlugin = ((LV2Platform*) mPlatform)->getPlugin(uri);
+	if(!mPlugin)
+		return;
+}
+
+bool LV2Adaptor::init() {
+	if(!mPlugin)
+		return false;
+
+	return mPlugin->init();
+}
+
 
--- framework/hosting/lv2/LV2Adaptor.h
+++ framework/hosting/lv2/LV2Adaptor.h
@@ -1,11 +1,30 @@
 #ifndef LV2ADAPTOR_H
 #define LV2ADAPTOR_H
+#include "framework/common.h"
+#include "framework/rack/UnitConnectors.h"
+#include "LV2Platform.h"
 
 namespace RackoonIO {
 namespace Hosting {
 
 class LV2Adaptor {
+public:
+	LV2Adaptor();
 
+	void setPlatform(const LV2Platform*);
+
+	bool init();
+	FeedState feed(PcmSample*);
+
+protected:
+	std::unique_ptr<LV2Plugin> mPlugin;
+	void loadPlugin(std::string);
+
+private:
+	std::map<std::string, SeqJack> jacks;
+	std::map<std::string, Plug> plugs;
+
+	const LV2Platform *mPlatform;
 };
 
 } // Hosting

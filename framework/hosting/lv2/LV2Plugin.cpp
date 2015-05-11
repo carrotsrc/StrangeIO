aff354b0a6255810ebbce451413d3e13470e4ab4
Charlie FG <cfg@zunautica.org>
2015-05-11 14:31:04 +0100

    Bulk out the Plugin class

--- framework/hosting/lv2/LV2Plugin.cpp
+++ framework/hosting/lv2/LV2Plugin.cpp
@@ -4,8 +4,15 @@ using namespace RackoonIO::Hosting;
 
 LV2Plugin::LV2Plugin(const LilvPlugin *p, const LV2NodeFactory *f) {
 	plugin = p;
+	inst = nullptr;
 	nodeFactory = f;
+
+	mFs = 44100;
 	profilePorts();
+}
+LV2Plugin::~LV2Plugin() {
+	if(inst)
+		lilv_instance_free(inst);
 }
 
 const LilvPlugin* LV2Plugin::getPlugin() {
@@ -24,11 +31,28 @@ std::string LV2Plugin::getUri() {
 	return std::string(lilv_node_as_string(lilv_plugin_get_uri(plugin)));
 }
 
+void LV2Plugin::setFs(double rate) {
+	mFs = rate;
+}
+
+double LV2Plugin::getFs() {
+	return mFs;
+}
+
+void LV2Plugin::init() {
+	if(!inst)
+		return;
+
+	lilv_instance_activate(inst);
+}
+
 void LV2Plugin::profilePorts() {
 	auto n = getNumPorts();
+	
 	auto nodeIn = nodeFactory->newUri(LILV_URI_INPUT_PORT);
 	auto nodeOut = nodeFactory->newUri(LILV_URI_OUTPUT_PORT);
 	auto nodeControl = nodeFactory->newUri(LILV_URI_CONTROL_PORT);
+
 	for(uint32_t i = 0; i < n; i++) {
 		auto port = lilv_plugin_get_port_by_index(plugin, i);
 		
@@ -41,4 +65,11 @@ void LV2Plugin::profilePorts() {
 			output.insert(std::pair<std::string, const LilvPort *>(std::string(pname), port));
 		}
 	}
+}
+
+bool LV2Plugin::instantiate() {
+	if(lilv_plugin_instantiate(plugin, mFs, NULL) == NULL)
+		return false;
+
+	return true;
 }
--- framework/hosting/lv2/LV2Plugin.h
+++ framework/hosting/lv2/LV2Plugin.h
@@ -9,17 +9,26 @@ namespace Hosting {
 class LV2Plugin {
 public:
 	LV2Plugin(const LilvPlugin*, const LV2NodeFactory*);
+	~LV2Plugin();
 	const LilvPlugin* getPlugin();
 
 	std::string getName();
 	uint32_t getNumPorts();
 	std::string getUri();
 
+	void setFs(double);
+	double getFs();
+
+	bool instantiate();
+	void init();
+
 protected:
 	const LV2NodeFactory *nodeFactory;
+	double mFs;
 
 private:
 	const LilvPlugin* plugin;
+	LilvInstance* inst;
 	std::map<std::string, const LilvPort*> input;
 	std::map<std::string, const LilvPort*> output;
 

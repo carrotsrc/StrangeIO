fb7bcae6e3b027ff025f8209eb4701523ad6aec7
Charlie FG <cfg@zunautica.org>
2015-05-11 14:31:19 +0100

    Add Adaptor class

---
+++ framework/hosting/lv2/LV2Adaptor.cpp
@@ -0,0 +1,5 @@
+#include "LV2Adaptor.h"
+
+using namespace RackoonIO::Hosting;
+
+
---
+++ framework/hosting/lv2/LV2Adaptor.h
@@ -0,0 +1,13 @@
+#ifndef LV2ADAPTOR_H
+#define LV2ADAPTOR_H
+
+namespace RackoonIO {
+namespace Hosting {
+
+class LV2Adaptor {
+
+};
+
+} // Hosting
+} // RackoonIO
+#endif

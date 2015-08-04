87981b7138d0e78c48c4c3742c2ad92480eda412
Charlie FG <cfg@zunautica.org>
2015-08-04 18:57:39 +0100

    Update test for DelayBuffer

--- test/DelayBuffer.cpp
+++ test/DelayBuffer.cpp
@@ -1,7 +1,7 @@
 #include "framework/buffers/DelayBuffer.h"
 #include "framework/common.h"
 
-void dprint(const int *d) {
+void dprint(const PcmSample *d) {
 	std::cout << "---" << std::endl;
 	for(int i = 0; i < 8; i++)
 		std::cout << d[i] << " ";
@@ -10,28 +10,28 @@ void dprint(const int *d) {
 }
 
 int main( void ) {
-	RackoonIO::Buffers::DelayBuffer<int> buffer(8);
+	RackoonIO::Buffers::DelayBuffer buffer(8);
 
-	int testA[22], testB[2];
+	PcmSample testA[22], testB[2];
 	int j = 1;
 	for(int i = 0; i < 22; i++)
 		testA[i] = j++;
 
 
-	RackoonIO::Buffers::DelayBuffer<int>::State cState = RackoonIO::Buffers::DelayBuffer<int>::OK;
-	if(buffer.supply(testA, 22) == RackoonIO::Buffers::DelayBuffer<int>::OK)
+	auto cState = RackoonIO::Buffers::DelayBuffer::OK;
+	if(buffer.supply(testA, 22) == RackoonIO::Buffers::DelayBuffer::OK)
 		std::cout << "testA OK" << std::endl;
 	else
 		std::cout << "testA WAIT" << std::endl;
 
-	while(cState != RackoonIO::Buffers::DelayBuffer<int>::WAIT) {
+	while(cState != RackoonIO::Buffers::DelayBuffer::WAIT) {
 		for(int i = 0; i < 2; i++)
 			testB[i] = j++;
 
 		cState = buffer.supply(testB, 2);
 	}
 	std::cout << "Full on j: " << j << std::endl;
-	const int *flush = buffer.flush();
+	const PcmSample* flush = buffer.flush();
 	dprint(flush);
 	std::cout << "Done!" << std::endl;
 

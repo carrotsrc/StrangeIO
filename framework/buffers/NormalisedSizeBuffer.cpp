c1c68d62e193696eadbbf911d1065f77b98970dc
Charlie FG <cfg@zunautica.org>
2015-08-04 18:31:10 +0100

    Switch buffer to using c++ memory functions
    The buffer is no longer a template class. The
    flush method takes a pointer to a PcmSample
    array which is written to directly, instead
    of the object returning a pointer to its own
    internal buffer.

--- framework/buffers/NormalisedSizeBuffer.cpp
+++ framework/buffers/NormalisedSizeBuffer.cpp
@@ -14,5 +14,102 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include "NormalisedSizeBuffer.h"
+#include <iterator>
+using namespace RackoonIO::Buffers;
+/** Constructor for a buffer
+ * 
+ * The constructor initialises the buffer to the normal period size and
+ * the overflow to leave room for the variable sized input.
+ *
+ * @param normal The normalised number of samples to output
+ * @param overflow The number of samples of extra space for variable sized input periods
+ */
+NormalisedSizeBuffer::NormalisedSizeBuffer ( int normal, int overflow ) {
+	nSize = normal;
+	buffer = new PcmSample[nSize];
+	remainder = new PcmSample[nSize+overflow];
 
+	bound = remainder + (nSize+overflow);
+	rWrite = rRead = remainder;
+	load = 0;
+	state = PARTIAL;
+}
+
+/** Supply a variable size period to the buffer
+ *
+ * The period is pSize number of samples located
+ * at location *period. The method returns the state
+ * of the buffer after supply unless OVERFLOW,
+ * which means that the supply overflow the read pointer;
+ * OVERFLOW means the period has not been copied.
+ *
+ * @param period A pointer to a period
+ * @param pSize The number of samples/frames in the period
+ * @return DISPATCH if the final load is greater than a normal period; PARTIAL if less
+ */
+NormalisedSizeBuffer::State NormalisedSizeBuffer::supply (const PcmSample* period, int pSize) {
+	if(rWrite < rRead && (rWrite + pSize) > rRead)
+		return OVERFLOW;
+
+	int delta = pSize;
+	PcmSample *pRead = (PcmSample*) period;
+
+	if(rWrite + pSize >= bound) {
+		delta = bound - rWrite;
+		std::copy(pRead, pRead+delta, rWrite);
+		rWrite = remainder;
+		pRead += delta;
+		delta = pSize-delta;
+
+	}
+
+	std::copy(pRead, pRead+delta, rWrite);
+	rWrite += delta;
+
+	load += pSize;
+
+	state = (load >= nSize) ? DISPATCH : PARTIAL;
+	return state;
+}
+
+/** Flush out a normal sized period of samples
+ *
+ * If there are samples waiting to be dispatched then
+ * calling this method will return a pointer to the 
+ * samples
+ *
+ * @param period A PcmSample pointer to the period to flush the samples into
+ *
+ */
+void NormalisedSizeBuffer::flush (PcmSample *period) {
+	if(state == PARTIAL || load < nSize)
+		return;
+
+	int delta = nSize;
+	PcmSample* bWrite = period;
+
+	if(rRead + nSize >= bound) {
+		delta = bound - rRead;
+		std::copy(rRead, rRead+delta, bWrite);
+		rRead = remainder;
+		bWrite += delta;
+		delta = nSize - delta;
+	}
+
+	std::copy(rRead, rRead+delta, bWrite);
+	rRead += delta;
+	load -= nSize;
+	state = (load >= nSize) ? DISPATCH : PARTIAL;
+
+	//return buffer;
+}
+
+
+/** Retrieve the current state of the buffer
+ *
+ * @return The current state
+ */
+NormalisedSizeBuffer::State NormalisedSizeBuffer::getState () {
+	return state;
+}
 // test
--- framework/buffers/NormalisedSizeBuffer.h
+++ framework/buffers/NormalisedSizeBuffer.h
@@ -45,7 +45,6 @@ namespace Buffers {
  * this buffer can be given a pointer to a cache object to flush into
  * directly instead of two memcpys to get it into cache
  */
-template<typename T>
 class NormalisedSizeBuffer {
 public:
 	/// The various stats the buffer will take on
@@ -55,9 +54,9 @@ public:
 		OVERFLOW ///< The write head has caught up with the read head (Shouldn't happen)
 	};
 
-	NormalisedSizeBuffer(int, int);
-	State supply(const T*, int);
-	const T *flush();
+	NormalisedSizeBuffer(int normal, int overflow);
+	State supply(const PcmSample*, int);
+	void flush(PcmSample *period);
 
 	void reset() { state = PARTIAL; };
 	State getState();
@@ -66,113 +65,13 @@ private:
 	int load;
 	int nSize;
 
-	T *buffer, *remainder, *rRead, *rWrite, *bound;
+	PcmSample *buffer, *remainder, *rRead, *rWrite, *bound;
 	State state;
 };
 
 /*
  * Template definitions
  */
-
-/** Constructor for a buffer
- * 
- * The constructor initialises the buffer to the normal period size and
- * the overflow to leave room for the variable sized input.
- *
- * @param normal The normalised number of samples to output
- * @param overflow The number of samples of extra space for variable sized input periods
- */
-template<typename T>
-NormalisedSizeBuffer<T>::NormalisedSizeBuffer ( int normal, int overflow ) {
-	nSize = normal;
-	buffer = (T*) malloc(sizeof(T)*nSize);
-	remainder = (T*) malloc(sizeof(T)*nSize+overflow);
-	bound = remainder + (nSize+overflow);
-	rWrite = rRead = remainder;
-	load = 0;
-	state = PARTIAL;
-}
-
-/** Supply a variable size period to the buffer
- *
- * The period is pSize number of samples located
- * at location *period. The method returns the state
- * of the buffer after supply unless OVERFLOW,
- * which means that the supply overflow the read pointer;
- * OVERFLOW means the period has not been copied.
- *
- * @param period A pointer to a period
- * @param pSize The number of samples/frames in the period
- * @return DISPATCH if the final load is greater than a normal period; PARTIAL if less
- */
-template<typename T>
-typename NormalisedSizeBuffer<T>::State
-NormalisedSizeBuffer<T>::supply (const T* period, int pSize) {
-	if(rWrite < rRead && (rWrite + pSize) > rRead)
-		return OVERFLOW;
-
-	int delta = pSize;
-	T *pRead = (T*) period;
-
-	if(rWrite + pSize >= bound) {
-		delta = bound - rWrite;
-		memcpy(rWrite, pRead, delta * sizeof(T)); 
-		rWrite = remainder;
-		pRead += delta;
-		delta = pSize-delta;
-
-	}
-
-	memcpy(rWrite, pRead, delta*sizeof(T));
-	rWrite += delta;
-
-	load += pSize;
-
-	state = (load >= nSize) ? DISPATCH : PARTIAL;
-	return state;
-}
-/** Flush out a normal sized period of samples
- *
- * If there are samples waiting to be dispatched then
- * calling this method will return a pointer to the 
- * samples
- *
- * @return A pointer to the samples being flushed from the buffer
- */
-template<typename T>
-const T *NormalisedSizeBuffer<T>::flush () {
-	if(state == PARTIAL || load < nSize)
-		return nullptr;
-
-	int delta = nSize;
-	T* bWrite = buffer;
-
-	if(rRead + nSize >= bound) {
-		delta = bound - rRead;
-		memcpy(bWrite, rRead, delta * sizeof(T));
-		rRead = remainder;
-		bWrite += delta;
-		delta = nSize - delta;
-	}
-
-	memcpy(bWrite, rRead, delta * sizeof(T));
-	rRead += delta;
-	load -= nSize;
-	state = (load >= nSize) ? DISPATCH : PARTIAL;
-
-	return buffer;
-}
-
-
-/** Retrieve the current state of the buffer
- *
- * @return The current state
- */
-template<typename T>
-typename NormalisedSizeBuffer<T>::State
-NormalisedSizeBuffer<T>::getState () {
-	return state;
-}
 
 
 } // Buffers

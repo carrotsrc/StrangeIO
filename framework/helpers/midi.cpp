ed12e86d6264872e05a0fc58dd1d7fd495b8b251
Charlie FG <cfg@zunautica.org>
2015-08-06 22:18:15 +0100

    Add routines for normalising midi velocity
    The velocity can be converted to
    -1.0 to +1.0
    or
     0.0 to +1.0

---
+++ framework/helpers/midi.cpp
@@ -0,0 +1,21 @@
+#include "midi.h"
+using namespace RackoonIO::Helpers;
+float MidiRoutines::normaliseVelocity64(int value) {
+	if(value == 64) {
+		return 0.0f;
+	} else {
+		return ( (value < 64.0f) ?
+			-((64.0f-value)*(0.015625f)) :
+			((value-63.0f)*(0.015625)));
+	}
+	
+}
+
+float MidiRoutines::normaliseVelocity128(int value) {
+	if(value == 64) {
+		return 0.5f;
+	} else {
+		return value/127.0f;
+	}
+	
+}
---
+++ framework/helpers/midi.h
@@ -0,0 +1,13 @@
+#ifndef __MIDI_H_1438891287__
+#define __MIDI_H_1438891287__
+ 
+#include "framework/common.h"
+namespace RackoonIO {
+namespace Helpers {
+namespace MidiRoutines {
+	float normaliseVelocity64(int value);
+	float normaliseVelocity128(int value);
+}
+} // helpers
+} // RackoonIO
+#endif

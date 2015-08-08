245343c641ba3c13147a13cac9886a9fdd7361c1
Charlie FG <cfg@zunautica.org>
2015-08-08 22:11:56 +0100

    Describe rack configuration in a struct tree
    The configuration is being separated out into a
    separate are of the system. The description of
    the rack is parsed into the struct tree, which
    will be used to setup the rack.
    
    This means the rack configuration is taken out
    of the the rack class and implemented externally

---
+++ framework/rack/setup/RackConfig.h
@@ -0,0 +1,7 @@
+#ifndef __RACKCONFIG_H_1439063000__
+#define __RACKCONFIG_H_1439063000__
+
+class RackConfig {
+
+}
+#endif
---
+++ framework/rack/setup/RackDesc.h
@@ -0,0 +1,61 @@
+#ifndef __RACKDESC_H_1439063050__
+#define __RACKDESC_H_1439063050__
+
+#include "framework/common.h"
+
+struct RackDesc {
+
+	typedef struct {
+
+		typedef struct {
+			int num_workers;
+		} Threading;
+		Threading threads;
+
+	} System;
+
+	typedef struct {
+
+		typedef struct {
+			std::string label;
+			std::string port;
+		} Controller;
+
+		std::vector<Controller> controllers;
+
+	} Midi;
+
+	typedef struct {
+
+		typedef struct {
+			std::string from, plug, to, jack;
+		} Link;
+
+		typedef struct {
+
+			typedef struct {
+				std::string type, value;
+			} Config;
+
+			typedef struct {
+				std::string name, module, code;
+			} Binding;
+
+			std::string label, unit, library;
+			std::vector<Config> configs;
+			std::vector<Binding> bindings;
+		} Unit;
+
+		std::vector<std::string> mainlines;
+		std::vector<Link> daisychains;
+		std::vector<Unit> units;
+
+	} Setup;
+
+	System system;
+	Midi midi;
+	Setup setup;
+
+
+};
+#endif

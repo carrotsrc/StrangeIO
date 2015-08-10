41c4d6c55c63eb1cbf10ab5b774c9062b8693d00
Charlie FG <cfg@zunautica.org>
2015-08-10 17:24:41 +0100

    Add assembly methods for rack

--- framework/rack/config/RackAssembler.cpp
+++ framework/rack/config/RackAssembler.cpp
@@ -1,8 +1,42 @@
 #include "RackAssembler.h"
+using namespace StrangeIO;
 using namespace StrangeIO::Config;
+
 RackAssembler::RackAssembler(std::unique_ptr<RackUnitFactory> factory) {
 	mUnitFactory = std::move(factory);
 }
-void RackAssembler::assemble(const RackDesc & desc, Rack & rack) {
+
+void RackAssembler::assemble(const RackDesc& desc, Rack& rack) {
 	
+}
+
+void RackAssembler::assembleMainlines(const RackDesc& desc, Rack& rack) {
+	for(auto mainline : desc.setup.mainlines) {
+		rack.addMainline(mainline);
+	}
+}
+
+void RackAssembler::assembleDaisychains(const RackDesc& desc, Rack& rack) {
+	for(auto link : desc.setup.daisychains) {
+		auto& udFrom = unitDescription(desc, link.from);
+		auto unitFrom = assembleUnit(udFrom.unit, udFrom.label, udFrom.library); 
+	}
+}
+
+const UnitDesc& RackAssembler::unitDescription(const RackDesc& desc, std::string label) {
+	for(const auto& unit : desc.setup.units) {
+		if(unit.label == label) {
+			return unit;
+		}
+	}
+
+	throw;
+}
+
+std::unique_ptr<RackUnit> RackAssembler::assembleUnit(std::string unit, std::string label, std::string target) {
+	if(target.empty()) {
+		return mUnitFactory->build(unit, label);
+	} else {
+		return mUnitFactory->load(target, unit, label);
+	}
 }
--- framework/rack/config/RackAssembler.h
+++ framework/rack/config/RackAssembler.h
@@ -3,20 +3,31 @@
 #include "framework/common.h"
 #include "framework/rack/Rack.h"
 #include "framework/rack/RackUnit.h"
+#include "framework/factories/RackUnitGenericFactory.h"
 
 #include "RackDesc.h"
+
 namespace StrangeIO {
 namespace Config {
 
 using RackUnitFactory = StrangeIO::RackUnitGenericFactory;
+using UnitDesc = RackDesc::Setup::Unit;
+
 
 class RackAssembler {
 public:
 	RackAssembler(std::unique_ptr<RackUnitFactory>);
-	void assemble(const RackDesc & desc, Rack & rack);
+	void assemble(const RackDesc& desc, Rack& rack);
+
+	std::unique_ptr<RackUnit> assembleUnit(std::string unit, std::string label, std::string target);
 
 private:
 	std::unique_ptr<RackUnitFactory> mUnitFactory;
+
+	void assembleMainlines(const RackDesc& desc, Rack& rack);
+	void assembleDaisychains(const RackDesc& desc, Rack& rack);
+
+	const UnitDesc& unitDescription(const RackDesc& desc, std::string label);
 };
 
 } // Config

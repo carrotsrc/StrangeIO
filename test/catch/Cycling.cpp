5b98448297b59bad4bb1c0d323ae4ab98fc41437
Charlie FG <cfg@zunautica.org>
2015-08-11 19:25:45 +0100

    Add test for unit connector

--- test/catch/Cycling.cpp
+++ test/catch/Cycling.cpp
@@ -6,31 +6,38 @@
 #include "framework/rack/RackUnit.h"
 #include "framework/rack/config/RackDocument.h"
 #include "framework/rack/config/RackAssembler.h"
-
+#include "units/TestingUnit.h"
 #if !DEVBUILD
 	#error The testing suite requires DEVBUILD to be enabled
 #endif
 
+using namespace StrangeIO;
 using namespace StrangeIO::Config;
+using namespace StrangeIO::Testing;
 
-TEST_CASE( "Test UnitConnectors", "[UnitConnectors]" ) {
-	RackDocument doc;
 
-	auto config = doc.load("basic.cfg");
+TEST_CASE( "Test Feed", "[UnitConnectors]" ) {
 
-	SECTION ( "Checking system" ) {
-		REQUIRE( config->system.threads.num_workers == 3 );
+	int feedCheck = 0;
+	Config::RackAssembler as(std::unique_ptr<RackUnitGenericFactory>(new RackUnitGenericFactory()));
+	auto unit = as.assembleUnit("MainlineUnit", "mainline", "units/MainlineUnit.rso");
+	auto unitPtr = unit.get();
+
+	Plug p(nullptr);
+	SeqJack j("audio", unitPtr);
+	p.jack = &j;
+	p.unit = unitPtr;
+
+
+	(static_cast<TestingUnit*>(unitPtr))->setFeedCheck(&feedCheck);
+	SECTION ( "Testing feed" ) {
+		auto state = p.jack->feed(nullptr);
+		REQUIRE(state == FEED_OK);
+		REQUIRE(feedCheck == 1);
 	}
 
 }
 
 TEST_CASE( "Test Unit Cycle", "[UnitCycling]" ) {
-	RackDocument doc;
-
-	auto config = doc.load("basic.cfg");
-
-	SECTION ( "Checking system" ) {
-		REQUIRE( config->system.threads.num_workers == 3 );
-	}
 
 }

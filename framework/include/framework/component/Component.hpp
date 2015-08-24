0e19ea3cb2498314230c5ace8cb9771645e94db0
Charlie FG <cfg@zunautica.org>
2015-08-24 12:18:36 +0100

    Add component states

--- framework/include/framework/component/Component.hpp
+++ framework/include/framework/component/Component.hpp
@@ -1,11 +1,16 @@
 #ifndef __COMPONENT_HPP_1440410429__
 #define __COMPONENT_HPP_1440410429__
-#include "framework/fwcommon.h"
+#include "framework/fwcommon.hpp"
 
 namespace StrangeIO {
 namespace Component {
+
 enum UnitType {
 	Mainliner, Passer, Combiner, Splitter, Dispatcher
+};
+
+enum ComponentState {
+	Active, Inactive
 };
 
 }

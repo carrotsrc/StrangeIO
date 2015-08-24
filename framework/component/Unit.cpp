3c9cfae1362d5cc3d5a639e0f6f8eca61c25882d
Charlie FG <cfg@zunautica.org>
2015-08-24 12:18:52 +0100

    Setup unit initial state and getters

--- framework/component/Unit.cpp
+++ framework/component/Unit.cpp
@@ -2,5 +2,22 @@
 
 using namespace StrangeIO::Component;
 
-Unit::Unit() {
+Unit::Unit(UnitType utype, std::string umodel, std::string ulabel) : 
+m_utype(utype), m_umodel(umodel), m_ulabel(ulabel), m_cstate(Inactive)
+{ }
+
+UnitType Unit::utype() const {
+	return m_utype;
+}
+
+std::string Unit::umodel() const {
+	return m_umodel;
+}
+
+std::string Unit::ulabel() const {
+	return m_ulabel;
+}
+
+ComponentState Unit::cstate() const {
+	return m_cstate;
 }
--- framework/include/framework/component/Unit.hpp
+++ framework/include/framework/component/Unit.hpp
@@ -1,14 +1,28 @@
 #ifndef __UNIT_HPP_1440406068__
 #define __UNIT_HPP_1440406068__
+
+#include "framework/component/Component.hpp"
+
 namespace StrangeIO {
 namespace Component {
 
 class Unit {
 public:
-	Unit();
+	Unit(UnitType utype, std::string umodel, std::string ulabel);
+	
+	UnitType utype() const;
+	std::string umodel() const;
+	std::string ulabel() const;
+
+	ComponentState cstate() const;
+
+protected:
 
 private:
+	const UnitType m_utype;
+	const std::string m_umodel, m_ulabel;
 
+	ComponentState m_cstate;
 
 };
 

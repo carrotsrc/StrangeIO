5eee3fe05820c5f29088b382bdadb9e85536b514
Charlie FG <cfg@zunautica.org>
2015-08-24 11:20:08 +0100

    Add namespace header

---
+++ framework/include/framework/component/Component.hpp
@@ -0,0 +1,13 @@
+#ifndef __COMPONENT_HPP_1440410429__
+#define __COMPONENT_HPP_1440410429__
+#include "framework/fwcommon.h"
+
+namespace StrangeIO {
+namespace Component {
+enum UnitType {
+	Mainliner, Passer, Combiner, Splitter, Dispatcher
+};
+
+}
+}
+#endif

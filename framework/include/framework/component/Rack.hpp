414ab7b87ee21f3f6cd2b7037678ff7c1edc7c2d
Charlie FG <cfg@zunautica.org>
2015-08-24 14:04:33 +0100

    Add Rack component

---
+++ framework/include/framework/component/Rack.hpp
@@ -0,0 +1,15 @@
+#ifndef __RACK_HPP_1440421209__
+#define __RACK_HPP_1440421209__
+#include "framework/component/Component.hpp"
+
+namespace StrangeIO {
+namespace Component {
+
+class Rack {
+	
+}
+
+} // Component
+} // StrangeIO
+#endif
+

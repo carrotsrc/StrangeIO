3f3375f51b32239bf8084ced72a5889844680fc9
Charlie FG <cfg@zunautica.org>
2015-08-25 10:19:54 +0100

    Implement beginning of cycle process

--- framework/component/Unit.cpp
+++ framework/component/Unit.cpp
@@ -1,9 +1,11 @@
 #include "framework/component/Unit.hpp"
+#include "framework/component/Rack.hpp"
 
 using namespace StrangeIO::Component;
 
-Unit::Unit(UnitType utype, std::string umodel, std::string ulabel) : 
-m_utype(utype), m_umodel(umodel), m_ulabel(ulabel), m_cstate(Inactive)
+Unit::Unit(UnitType utype, std::string umodel, std::string ulabel) :
+m_utype(utype), m_umodel(umodel), m_ulabel(ulabel), m_cstate(ComponentState::Inactive),
+m_rack(nullptr)
 { }
 
 UnitType Unit::utype() const {
@@ -27,6 +29,52 @@ void Unit::change_cstate(ComponentState state) {
 }
 
 
-void Unit::register_metric(MetricType type, int value) {
+void Unit::register_metric(ProfileMetric type, int value) {
+	if(!m_rack) return;
 
+	m_rack->register_metric(type,value);
+}
+
+void Unit::set_rack(Rack* ptr_rack) {
+	m_rack = ptr_rack;
+}
+
+LineProfile& Unit::profile() {
+	return m_profile;
+}
+
+CycleState Unit::run_cycle(CycleType type) {
+
+	auto state = CycleState::Complete;
+	switch(type) {
+	case CycleType::Ac:
+		state = cycle();
+		break;
+
+	case CycleType::Sync:
+		sync_line(m_profile);
+		break;
+
+	case CycleType::Warmup:
+		state = init();
+		break;
+	}
+
+	if(state > CycleState::Complete) {
+		return state;
+	}
+
+	return state;
+}
+
+CycleState Unit::cycle() {
+	return CycleState::Complete;
+}
+
+CycleState Unit::init() {
+	return CycleState::Complete;
+}
+
+void Unit::sync_line(LineProfile & profile) {
+	return;
 }
--- framework/include/framework/component/Unit.hpp
+++ framework/include/framework/component/Unit.hpp
@@ -6,26 +6,39 @@
 namespace StrangeIO {
 namespace Component {
 
+class Rack; // Forward declaration
+
 class Unit {
 public:
 	Unit(UnitType utype, std::string umodel, std::string ulabel);
 	
+	// Description and State methods
 	UnitType utype() const;
 	std::string umodel() const;
 	std::string ulabel() const;
-
 	ComponentState cstate() const;
+
+
+	// System methods
+	void set_rack(Rack* ptr_rack);
+	CycleState run_cycle(CycleType cycle);
 
 protected:
 	void change_cstate(ComponentState state);
-	void register_metric(MetricType type, int value);
+	void register_metric(ProfileMetric type, int value);
+	LineProfile& profile();
+
+	CycleState cycle();
+	CycleState init();
 
 private:
 	const UnitType m_utype;
 	const std::string m_umodel, m_ulabel;
-
 	ComponentState m_cstate;
+	Rack* m_rack;
+	LineProfile m_profile;
 
+	void sync_line(LineProfile & profile);
 };
 
 } // Component

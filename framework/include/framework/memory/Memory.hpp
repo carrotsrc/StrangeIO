3967b6112083d0c4108c285e5603b750300d6b47
Charlie FG <cfg@zunautica.org>
2015-08-28 11:59:15 +0100

    Close decl

--- framework/include/framework/memory/Memory.hpp
+++ framework/include/framework/memory/Memory.hpp
@@ -1,12 +1,13 @@
 #ifndef __MEMORY_HPP_1440757499__
 #define __MEMORY_HPP_1440757499__
-
+#include "framework/fwcommon.hpp"
 namespace StrangeIO {
 namespace Memory {
 
 	struct CacheHandle {
-		
-	}
+		PcmSample* ptr;
+		int num_blocks;
+	};
 }
 }
 

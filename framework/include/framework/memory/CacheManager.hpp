2518961351116be421c8f556f6483e51cdefd234
Charlie FG <cfg@zunautica.org>
2015-08-28 11:59:27 +0100

    Start handling a vector of cache handles

--- framework/include/framework/memory/CacheManager.hpp
+++ framework/include/framework/memory/CacheManager.hpp
@@ -1,6 +1,10 @@
 #ifndef __CACHEMANAGER_HPP_1440753739__
 #define __CACHEMANAGER_HPP_1440753739__
+
+#include <vector>
+#include "framework/memory/Memory.hpp"
 #include "framework/memory/CacheUtilityInterface.hpp"
+
 
 namespace StrangeIO {
 namespace Memory {	
@@ -15,15 +19,17 @@ public:
 
 	void cache_alloc(int block_size);
 	size_t cache_size() const;
-	int num_blocks() const;
-	int block_size() const;
+	unsigned int num_blocks() const;
+	unsigned int block_size() const;
 
 protected:
 
 private:
-	PcmSample* m_cache;
-	int m_num_blocks, m_block_size;
+	PcmSample* m_raw_cache;
+	unsigned int m_num_blocks, m_block_size;
 	size_t m_cache_size;
+	
+	std::vector<CacheHandle> m_blocks_ready, m_blocks_inuse;
 };
 
 }
--- framework/memory/CacheManager.cpp
+++ framework/memory/CacheManager.cpp
@@ -3,12 +3,12 @@
 using namespace StrangeIO::Memory;
 
 CacheManager::CacheManager(int num_blocks) :
-m_cache(nullptr), m_num_blocks(num_blocks), m_block_size(0),
+m_raw_cache(nullptr), m_num_blocks(num_blocks), m_block_size(0),
 m_cache_size(0)
 { }
 
 CacheManager::~CacheManager() {
-	delete m_cache;
+	delete m_raw_cache;
 }
 
 PcmSample* CacheManager::alloc_raw(int num) {
@@ -19,16 +19,31 @@ size_t CacheManager::cache_size() const {
 	return m_cache_size;
 }
 
-int CacheManager::num_blocks() const {
+unsigned int CacheManager::num_blocks() const {
 	return m_num_blocks;
 }
-int CacheManager::block_size() const {
+
+unsigned int CacheManager::block_size() const {
 	return m_block_size;
 }
 
 void CacheManager::cache_alloc(int block_size) {
 	m_block_size = block_size;
 	
-	m_cache = new PcmSample[m_block_size*m_num_blocks];
+	m_raw_cache = new PcmSample[m_block_size*m_num_blocks];
 	m_cache_size = block_size*m_num_blocks;
+
+	m_blocks_ready.reserve(m_num_blocks);
+
+	auto ptr = m_raw_cache;
+	
+	for(auto i = 0u; i < m_num_blocks; i++) {
+		ptr += (i*m_block_size);
+		m_blocks_ready.push_back(
+			CacheHandle {
+				.ptr = ptr,
+				.num_blocks = 1
+			}
+		);
+	}
 }

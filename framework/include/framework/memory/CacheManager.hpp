e4f116c37243c1e73d08a08f87e150dac619b3ad
Charlie FG <cfg@zunautica.org>
2015-08-28 11:19:18 +0100

    Include definition of cache_alloc

--- framework/include/framework/memory/CacheManager.hpp
+++ framework/include/framework/memory/CacheManager.hpp
@@ -1,13 +1,19 @@
 #ifndef __CACHEMANAGER_HPP_1440753739__
 #define __CACHEMANAGER_HPP_1440753739__
 #include "framework/memory/CacheUtilityInterface.hpp"
+
+namespace StrangeIO {
+namespace Memory {	
+
+
 class CacheManager : public CacheUtilityInterface {
 public:
 	CacheManager(int num_blocks);
+	~CacheManager();
 	PcmSample* alloc_raw(int num);
 	void free_raw(PcmSample* ptr);
 
-	bool cache_alloc(size_t cache_size, int block_size);
+	void cache_alloc(int block_size);
 	size_t cache_size() const;
 	int num_blocks() const;
 	int block_size() const;
@@ -18,5 +24,8 @@ private:
 	PcmSample* m_cache;
 	int m_num_blocks, m_block_size;
 	size_t m_cache_size;
+};
+
+}
 }
 #endif
--- framework/memory/CacheManager.cpp
+++ framework/memory/CacheManager.cpp
@@ -1,8 +1,20 @@
 #include "framework/memory/CacheManager.hpp"
 
-PcmSample* CacheManager::alloc_raw(int num) {
-	
+using namespace StrangeIO::Memory;
+
+CacheManager::CacheManager(int num_blocks) :
+m_cache(nullptr), m_num_blocks(num_blocks), m_block_size(0),
+m_cache_size(0)
+{ }
+
+CacheManager::~CacheManager() {
+	delete m_cache;
 }
+
+PcmSample* CacheManager::alloc_raw(int num) {
+	return nullptr;
+}
+
 size_t CacheManager::cache_size() const {
 	return m_cache_size;
 }
@@ -12,4 +24,11 @@ int CacheManager::num_blocks() const {
 }
 int CacheManager::block_size() const {
 	return m_block_size;
+}
+
+void CacheManager::cache_alloc(int block_size) {
+	m_block_size = block_size;
+	
+	m_cache = new PcmSample[m_block_size*m_num_blocks];
+	m_cache_size = block_size*m_num_blocks;
 }

d92b48b18969f9ab189c35fabc06d8ef72af50b7
Charlie FG <cfg@zunautica.org>
2015-08-29 20:34:05 +0100

    Add classes for next cache testing

--- test/catch/Objects.hpp
+++ test/catch/Objects.hpp
@@ -76,16 +76,12 @@ public:
 		m_feed_count++;
 	}
 
-	void set_configuration(std::string key, std::string value) {
-		if(key == "test_key") {
-			m_config_test = std::stoi(value);
-		}
-	}
-
 	void delayed_constructor() {
 		add_input("power");
 		add_output("audio");
 	}
+	
+	void set_configuration(std::string, std::string) {}
 
 	CycleState cycle() {
 		feed_out(Memory::CachePtr((PcmSample*)0xFEED, 1, nullptr), OmegaAudio);
@@ -111,10 +107,6 @@ public:
 
 	int feed_count() const {
 		return m_feed_count;
-	}
-
-	int config_test() const {
-		return m_config_test;
 	}
 
 private:
@@ -185,8 +177,7 @@ public:
 	DeltaUnit(std::string label)
 	: Unit(UnitType::Combiner, "Delta", label),
 	m_init_count(0), m_feed_count(0), m_partial_count(0), 
-	m_feed_check(0.0f),
-	m_ca{Memory::CachePtr()}, m_cb{Memory::CachePtr()}
+	m_feed_check(0.0f)
 	{ 
 		add_input("channel_a");
 		add_input("channel_b");
@@ -199,10 +190,9 @@ public:
 	void feed_line(Memory::CachePtr samples, int id) {
 
 		if( id == DeltaCA ) {
-			//m_ca.swap(samples);
 			m_ca = samples;
 		} else if( id == DeltaCB) {
-			m_cb.swap(samples);
+			m_cb = samples;
 		}
 
 		m_feed_count++;
@@ -214,9 +204,9 @@ public:
 	CycleState cycle() {
 		auto state = CycleState::Complete;
 
-		if(input_connected(DeltaCA) && !m_ca) {
+		if(input_connected(DeltaCA) && m_ca == false) {
 			state = CycleState::Partial;
-		} else if(input_connected(DeltaCB) && !m_cb) {
+		} else if(input_connected(DeltaCB) && m_cb == false) {
 			state = CycleState::Partial;
 		} else {
 			feed_out(m_ca, DeltaAudio);
@@ -263,4 +253,96 @@ private:
 
 };
 
+#define PhiAudio 0
+#define PhiAudioIn 0
+class PhiUnit : public Unit {
+public:
+	PhiUnit(std::string label)
+	: Unit(UnitType::Mainliner, "Phi", label),
+	m_init_count(0), m_feed_count(0)
+	{ 
+		add_input("power");
+		add_output("audio");
+	};
+
+	void feed_line(Memory::CachePtr samples, int id) {
+		m_feed_count++;
+	}
+
+	CycleState cycle() {
+		auto samples = cache_alloc(5);
+		return CycleState::Complete;
+	}
+
+	CycleState init() {
+		m_init_count++;
+		return CycleState::Complete;
+	}
+
+	// Checks
+	int init_count() const {
+		return m_init_count;
+	}
+
+	int feed_count() const {
+		return m_feed_count;
+	}
+
+	int config_test() const {
+		return m_config_test;
+	}
+
+private:
+	int m_init_count, m_feed_count, m_config_test;
+
+};
+
+
+#define TauAudioIn 0
+class TauUnit : public Unit {
+public:
+	TauUnit(std::string label)
+	: Unit(UnitType::Dispatcher, "Tau", label),
+	m_init_count(0), m_feed_count(0)
+	{ 
+		add_input("audio_in");
+	};
+
+	void feed_line(Memory::CachePtr samples, int id) {
+		m_num_blocks = samples.num_blocks();
+		m_ptr = samples.get();
+		m_feed_count++;
+	}
+
+	CycleState cycle() {
+		return CycleState::Complete;
+	}
+
+	CycleState init() {
+		m_init_count++;
+		return CycleState::Complete;
+	}
+
+	// Checks
+	int init_count() const {
+		return m_init_count;
+	}
+
+	int feed_count() const {
+		return m_feed_count;
+	}
+
+	int num_blocks() const {
+		return m_num_blocks;
+	}
+	
+	const PcmSample* ptr() {
+		return m_ptr;
+	}
+
+private:
+	int m_init_count, m_feed_count, m_num_blocks;
+	const PcmSample* m_ptr;
+
+};
 

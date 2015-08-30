07d78fa2cd6044f1c63a4c10acd0741ee1402397
Charlie FG <cfg@zunautica.org>
2015-08-30 20:35:46 +0100

    Implement flush for Alsa input handle

--- framework/midi/AlsaInputHandle.cpp
+++ framework/midi/AlsaInputHandle.cpp
@@ -1,10 +1,20 @@
 #include "framework/midi/AlsaInputHandle.hpp"
-
+#include <iostream>
 using namespace StrangeIO::Midi;
 
 AlsaInputHandle::AlsaInputHandle(snd_rawmidi_t* handle, std::string port_name) :
 MidiInputHandle(port_name), m_handle(handle) { }
 
 MidiCode AlsaInputHandle::flush() {
-	return MidiCode{0};
+	MidiCode code;
+	if(auto status = snd_rawmidi_read(m_handle, (void*)&code, 3) < 0) {
+			if(status == -EAGAIN || status == -EBUSY)
+			return MidiCode{0};
+
+			std::cerr << port_name() << ": Critical error Reading Midi on port - " <<
+			snd_strerror(status) << std::endl;
+			return MidiCode{0};
+	}
+	
+	return code;
 }

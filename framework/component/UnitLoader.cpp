202de70d079c35abea140aa1f6f5d268a4b15653
Charlie FG <cfg@zunautica.org>
2015-08-31 14:58:50 +0100

    Skeleton compiles

--- framework/component/UnitLoader.cpp
+++ framework/component/UnitLoader.cpp
@@ -1,2 +1,7 @@
-#include "framework/component/UnitLoad.hpp"
+#include "framework/component/UnitLoader.hpp"
 
+using namespace StrangeIO::Component;
+
+unit_uptr UnitLoader::load(std::string unit, std::string name, std::string library) {
+	return nullptr;
+}
--- framework/include/framework/component/UnitLoader.hpp
+++ framework/include/framework/component/UnitLoader.hpp
@@ -2,14 +2,15 @@
 #define __UNITLOADER_HPP_1441029038__
 
 #include "framework/fwcommon.hpp"
+#include "framework/component/Component.hpp"
 #include "framework/component/Unit.hpp"
 
 namespace StrangeIO {
 namespace Component {
 
 class UnitLoader {
-	
-}
+	unit_uptr load(std::string unit, std::string name, std::string library);
+};
 
 } // Component
 } // StrangeIO

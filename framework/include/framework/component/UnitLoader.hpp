79832ba8ded83193e9ed5c9ff3c94d3f329b051c
Charlie FG <cfg@zunautica.org>
2015-08-31 14:54:13 +0100

    Add new Unit Loader class

---
+++ framework/include/framework/component/UnitLoader.hpp
@@ -0,0 +1,17 @@
+#ifndef __UNITLOADER_HPP_1441029038__
+#define __UNITLOADER_HPP_1441029038__
+
+#include "framework/fwcommon.hpp"
+#include "framework/component/Unit.hpp"
+
+namespace StrangeIO {
+namespace Component {
+
+class UnitLoader {
+	
+}
+
+} // Component
+} // StrangeIO
+ 
+#endif

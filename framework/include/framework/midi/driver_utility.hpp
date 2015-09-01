1159c1ccb599b313ff793a286e6d7e073bf209e4
Charlie FG <cfg@zunautica.org>
2015-09-01 11:32:43 +0100

    Update fwcommon

--- framework/include/framework/midi/driver_utility.hpp
+++ framework/include/framework/midi/driver_utility.hpp
@@ -1,7 +1,6 @@
 #ifndef __DRIVERUTILITYINTERFACE_HPP_1440954622__
 #define __DRIVERUTILITYINTERFACE_HPP_1440954622__
-#define MIDI_ALSA 1
-#if MIDI_ALSA
+#if ALSA_MIDI
 	#include "framework/midi/alsa_driver_utility.hpp"
 #endif
 #endif

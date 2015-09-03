9999514d807a2660ba28699cc4a38d84e8e3f406
Charlie FG <cfg@zunautica.org>
2015-09-03 13:23:27 +0100

    Test routines

--- test/catch/Objects.cpp
+++ test/catch/Objects.cpp
@@ -22,12 +22,66 @@
 	#error The testing suite requires DEVBUILD to be enabled
 #endif
 #define MidiControllerId 50
+using namespace strangeio;
+
+#include "framework/routine/sound.hpp"
+TEST_CASE( "Interleaving routines of two channels", "[strangeio::routine]" ) {
+	PcmSample interleaved[12] = {
+		0.50f, 0.10f,
+		0.51f, 0.11f,
+		0.52f, 0.12f,
+		0.53f, 0.13f,
+		0.54f, 0.14f,
+		0.55f, 0.15f
+		};
+
+	PcmSample out[12] = {0};
+	routine::sound::deinterleave2(interleaved, out, 6);
+
+	SECTION( "Deinterleave a two channel period of samples" ) {
+
+		REQUIRE( out[0] == 0.50f );
+		REQUIRE( out[5] == 0.55f );
+		REQUIRE( out[6] == 0.10f );
+		REQUIRE( out[11] == 0.15f );
+	}
+
+	PcmSample reinter[12] = {0};
+	routine::sound::interleave2(out, reinter, 6);
+
+	SECTION( "Interleave a two channel period of samples" ) {
+
+		REQUIRE( reinter[0] == 0.50f );
+		REQUIRE( reinter[1] == 0.10f );
+
+		REQUIRE( reinter[10] == 0.55f );
+		REQUIRE( reinter[11] == 0.15f );
+	}
+
+
+}
+
+#include "framework/routine/midi.hpp"
+
+TEST_CASE( "Convert velocity to a normalised range", "[strangeio::routine]" ) {
+
+	
+	SECTION( "Normalise to a range of -1.0 to +1.0" ) {
+		REQUIRE( routine::midi::normalise_velocity64(127) == 1.0f );
+		REQUIRE( routine::midi::normalise_velocity64(64) == 0.0f );
+		REQUIRE( routine::midi::normalise_velocity64(0) == -1.0f );
+	}
+
+	SECTION( "Normalise to a range of 0.0 to 1.0" ) {
+		REQUIRE( routine::midi::normalise_velocity128(127) == 1.0f );
+		REQUIRE( routine::midi::normalise_velocity128(64) == 0.5f );
+		REQUIRE( routine::midi::normalise_velocity128(0) == 0.0f );
+	}
+}
 
 #include "framework/component/rack.hpp"
-using namespace strangeio;
 #include "framework/memory/cache_manager.hpp"
 using namespace strangeio::memory;
-
 
 TEST_CASE("CacheManager", "[StrangeIO::Memory]") {
 	

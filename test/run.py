59422f9c6e8ab07a215a7a305da366b2da93a8b4
Charlie FG <cfg@zunautica.org>
2015-09-05 10:53:36 +0100

    Update run.py

---
+++ test/run.py
@@ -0,0 +1,74 @@
+#!/bin/env python
+import sys
+from os import environ
+from subprocess import call
+
+def print_help():
+		print("Usage: run.py [flags] [test test ...]")
+		print("flags\t: Any 'catch' flags that don't require an argument")
+		print("(test)\t: One or more of the test routines")
+		print("")
+		print("Namespace test routines:")
+		
+		print("component\t: Building, linking and cycling components")
+		print("config\t\t: Loading configs and assembling racks")
+		print("event\t\t: Creating and processing events")
+		print("memory\t\t: Cache and cache_ptr behaviour")
+		print("midi\t\t: Loading midi devices and processing signals")
+		print("routine\t\t: Helper routines for sound and midi")
+		print("strangeio\t\t: The root namespace objects like library loading")
+		print("thread\t\t: Running thread pool and processing tasks")
+		print("")
+		print("O/S Specific:")
+		print("linux\t\t: Self sustaining test rig for Linux")
+		exit();
+
+if len(sys.argv) == 1:
+    print_help()
+
+if sys.argv[1] == "-h" or sys.argv[1] == "--help":
+    print_help()
+
+if not environ.has_key('STRANGEFW'):
+    print "Error: environement STRANGEFW does not exist\n\n\tTry 'export STRANGEFW=path/to/framework'"
+    exit()
+
+target = sys.argv[1]
+environ['LD_LIBRARY_PATH'] = environ['STRANGEFW']
+
+if target == "all":
+    print("\n\n~~~ Running all tests ~~~\n")
+    args = ["./suite"]
+    if call(args) != 0:
+        print("### Test Failed ###")
+        exit(1)
+    print("~~~ Tests completed ~~~")
+else:
+    tags = []
+    flags = []
+    first = 1
+
+    for tag in sys.argv[1:]:
+        if tag[0] == "-":
+           flags = flags +[tag]
+           continue
+
+	if first == 1:
+           first = 0
+           tags = ["\""]
+	else:
+           tags = tags + [","]
+	if tag == "strangeio":
+		tags = tags + ["[strangeio]"]
+	else:
+		tags = tags + ["[strangeio::"+tag+"]"]
+    tags = tags + ["\""]
+
+    args = ["./suite"] + flags + tags
+
+    if call(args) != 0:
+            print("### Test Failed ###")
+            exit(1)
+
+    print("~~~ Test completed ~~~")
+

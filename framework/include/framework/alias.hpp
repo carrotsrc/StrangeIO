f8ad1d4970a82065a627c62a0fae66fde9eab393
Charlie FG <cfg@zunautica.org>
2015-09-06 19:14:54 +0100

    Add header for easier namespace aliases

---
+++ framework/include/framework/alias.hpp
@@ -0,0 +1,25 @@
+
+#ifndef STRANGIO_ALIAS_NAMESPACE
+#define STRANGIO_ALIAS_NAMESPACE
+
+namespace strangeio {
+	namespace component{}
+	namespace memory{}
+	namespace thread{}
+	namespace event{}
+	namespace config{}
+	namespace buffer{}
+	namespace routine{}
+	namespace midi{}
+}
+
+namespace siocom = strangeio::component;
+namespace siomem = strangeio::memory;
+namespace siothr = strangeio::thread;
+namespace sioevt = strangeio::event;
+namespace siocfg = strangeio::config;
+namespace siobuf = strangeio::buffer;
+namespace siortn = strangeio::routine;
+namespace siomid = strangeio::midi;
+
+#endif

cd8dca4dc9792ad56d9164561695c9c2e3eb97b5
Charlie FG <cfg@zunautica.org>
2015-09-10 09:15:07 +0100

    Update combine tests to check current behaviour
    The combine's behaviour has changed slightly and needs
    global syncs to change states correctly. Add global sync
    simulations to the test routines

--- test/catch_combine.cpp
+++ test/catch_combine.cpp
@@ -17,7 +17,9 @@ TEST_CASE("Test line activation", "[strangeio::spec],[strangeio::spec::combine]"
 	const_cast<siocom::LinkIn*>(unit.get_input(0))->connected = true;
 	const_cast<siocom::LinkIn*>(unit.get_input(1))->connected = true;
 
-
+	/* State change is inactive -> active
+	 * so no global sync is required here
+	 */
 	SECTION("Single line activation") {
 		unit.sync_line(profile, flags, 0);
 
@@ -44,15 +46,34 @@ TEST_CASE("Test line deactivation", "[strangeio::spec],[strangeio::spec::combine
 	siocom::sync_profile profile{0};
 	profile.state = (int)siocom::line_state::active;
 
+
 	auto flags = (siocom::sync_flag)siocom::sync_flags::upstream;
 	unit.sync_line(profile, flags, 0);
 	unit.sync_line(profile, flags, 1);
+
+	/* after an upstream sync, if the global state has changed, the dispatch
+	 * unit will trigger a global sync, so all the unit's global profiles
+	 * are the same. Here we simulate the global sync.
+	 */
+	unit.sync_line(profile, (siocom::sync_flag)siocom::sync_flags::glob_sync, 0);
+	unit.sync_line(profile, (siocom::sync_flag)siocom::sync_flags::glob_sync, 1);
+
+	
 	profile.state = (int)siocom::line_state::inactive;
 
 	SECTION("Single line deactivation") {
 		unit.sync_line(profile, flags, 0);
 
+		/* no global sync is necessary here -- not all lines have been
+		 * deactivated, meanning the combine unit will not propogate the upstream
+		 * inactive profile, which means the dispatch unit won't change state
+		 * and so there is no global sync; the rack remains active, while not
+		 * all lines and units are active.
+		 */
+
+		REQUIRE(unit.global_profile().state == (int)siocom::line_state::active);
 		REQUIRE(unit.unit_profile().state == (int)siocom::line_state::active);
+
 		REQUIRE(unit.db_input_active(0) == false);
 		REQUIRE(unit.db_input_active(1) == true);
 	}
@@ -61,7 +82,16 @@ TEST_CASE("Test line deactivation", "[strangeio::spec],[strangeio::spec::combine
 		unit.sync_line(profile, flags, 0);
 		unit.sync_line(profile, flags, 1);
 
+		/* once again, need to globally resync since both lines have been deactated
+		 * so the dispatch would receive the upstream inactivity sync, change state 
+		 * and trigger a global resync
+		 */
+		unit.sync_line(profile, (siocom::sync_flag)siocom::sync_flags::glob_sync, 0);
+		unit.sync_line(profile, (siocom::sync_flag)siocom::sync_flags::glob_sync, 1);
+
+		REQUIRE(unit.global_profile().state == (int)siocom::line_state::inactive);
 		REQUIRE(unit.unit_profile().state == (int)siocom::line_state::inactive);
+		
 		REQUIRE(unit.db_input_active(0) == false);
 		REQUIRE(unit.db_input_active(1) == false);
 	}

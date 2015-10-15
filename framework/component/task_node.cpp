105fe515daee9f4a28fc68409f5fa783c3e2f152
Charlie Fyvie-Gauld <cfg@zunautica.org>
2015-10-15 22:11:37 +0100

    Add task_node
    The task_node class will be used to automatically
    run pipelines on a worker thread. For example the
    combine spec unit will feed into a task_node which
    will continue running the pipeline in another thread.

---
+++ framework/component/task_node.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "framework/component/task_node.hpp"
+
+using namespace strangeio;
+using namespace strangeio::component;
+
+task_node::task_node ()
+	: thread::task_utility ()
+{ 
+	add_input("in");
+	add_output("out");
+}
+
+task_node::~task_node() {
+}
+
+void task_node::feed_line(memory::cache_ptr samples, int line) {
+	feed_out(samples, 0);
+}
+
+cycle_state task_node::cycle_line(cycle_type type) {
+	auto out = this->get_output(0);
+	add_task([out, type]() {
+		std::cout << "Split task" << std::endl;
+		out->to->unit->cycle_line(type);
+	});
+	return cycle_state::complete;
+}
+
---
+++ framework/include/framework/component/task_node.hpp
@@ -0,0 +1,27 @@
+#ifndef TASK_NODE_HPP
+#define	TASK_NODE_HPP
+
+#include "framework/component/linkable.hpp"
+#include "framework/thread/task_utility.hpp"
+namespace strangeio {
+namespace component {
+
+class task_node
+	: public linkable 
+	, public thread::task_utility
+	
+{
+public:
+	task_node();
+	virtual ~task_node();
+	cycle_state cycle_line(cycle_type type);
+	void feed_line(memory::cache_ptr samples, int line);
+
+protected:
+private:
+};
+
+}
+}
+#endif	/* TASK_NODE_HPP */
+

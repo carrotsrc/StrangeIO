5240e0d454a002a2e81107f78f5fe594b35d8f23
Charlie Fyvie-Gauld <cfg@zunautica.org>
2015-10-15 12:59:57 +0100

    Clean up normalised size buffer

--- framework/staging/buffer/NormalisedSizeBuffer.cpp
+++ framework/staging/buffer/NormalisedSizeBuffer.cpp
@@ -13,7 +13,7 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include "NormalisedSizeBuffer.h"
+//#include "NormalisedSizeBuffer.h"
 #include <iterator>
 using namespace strangeio::Buffers;
 /** Constructor for a buffer

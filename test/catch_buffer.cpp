fd45443252a2bdc3d95b708fc83b896c083700d9
Charlie Fyvie-Gauld <cfg@zunautica.org>
2016-01-02 23:09:40 +0000

    Add testing for circular buffer

--- test/catch_buffer.cpp
+++ test/catch_buffer.cpp
@@ -6,34 +6,79 @@
 
 #include "framework/routine/sound.hpp"
 
+#define IN_SIZE 518
+#define OUT_SIZE 512
+#define LOOP_NUM 1000
+
 TEST_CASE( "Test Circular buffer", "[strangeio::buffer],[strangeio::buffer::circular]" ) {
-	siomem::cache_manager cache(6);
-	siobuf::circular cb;
-	PcmSample samples_1[48], samples_2[16];
-	
-	cache.build_cache(32);	
-	siomem::cache_ptr cptr(cache.alloc_raw(1), 1, &cache);
-	
-	for(int i = 0u; i < 48; i++) {
-		samples_1[i] = i;
+
+strangeio::buffer::circular cb;
+cb.set_size(3*(IN_SIZE+4));
+
+auto iv = 1;
+auto ov = 1;
+PcmSample *buf_in = new PcmSample[IN_SIZE];
+PcmSample *buf_out = new PcmSample[OUT_SIZE];
+
+SECTION("Linear") {
+	for( auto i = 0u; i < LOOP_NUM; i++) {
+		if(cb.load() < OUT_SIZE) {
+			for(auto j = 0; j < IN_SIZE; j++) {
+				buf_in[j] = iv++;
+			}
+			cb.insert(buf_in, IN_SIZE);
+		}
+
+		cb.get(buf_out, OUT_SIZE);
+		for(auto k = 0u; k < OUT_SIZE; k++) {
+			REQUIRE(buf_out[k] == ov++);
+		}
 	}
+}
+
+SECTION("De/Interleaved") {
+	PcmSample *buf_work = new PcmSample[IN_SIZE];
+	auto ivo = 1;
+	for( auto i = 0u; i < LOOP_NUM; i++) {
+		
+		if(cb.load() < OUT_SIZE) {
+
+			auto frames = IN_SIZE/2;
+			
+			for(auto j = 0; j < frames; j++) {
+				buf_in[j] = iv;
+				buf_in[j+frames] = iv*-1;
+				iv++;
+			}
+			strangeio::routine::sound::interleave2(buf_in, buf_work, frames);
+
+			for(auto j = 0; j < IN_SIZE; j++) {
+				REQUIRE(buf_work[j++] == ivo);
+				REQUIRE(buf_work[j] == ivo*-1);
+				ivo++;
+			}
+
+			cb.insert(buf_work, IN_SIZE);
+		}
+
+		auto frames_out = OUT_SIZE/2;
+		cb.get(buf_out, OUT_SIZE);
 	
-	
-	
-	cb.set_size(48);
-	cb.set_overwrite(true);
-	cb.insert(samples_1, 48);
-	
-	cb.get(cptr);
-	
-	
-	REQUIRE(cptr[0] == 0);
-	REQUIRE(cptr[31] == 31);
-	
-	cb.get(cptr);
-	
-	REQUIRE(cptr[0] == 32);
-	REQUIRE(cptr[31] == 15);
-	
+		auto ot = ov;
+		for(auto j = 0; j < OUT_SIZE; j++) {
+			REQUIRE(buf_out[j++] == ot);
+			REQUIRE(buf_out[j] == (ot*-1));
+			ot++;
+		}
+		
+		strangeio::routine::sound::deinterleave2(buf_out, buf_work, frames_out);
+
+		for(auto j = 0; j < frames_out; j++) {
+			REQUIRE(buf_work[j] == ov);
+			REQUIRE(buf_work[j+frames_out] == ov*-1);
+			ov++;
+		}
+	}
+}
 
 }

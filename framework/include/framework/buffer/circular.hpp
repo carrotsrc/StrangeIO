d5b45598404ede42c2dfbf11fd0fff04e1059857
Charlie Fyvie-Gauld <cfg@zunautica.org>
2016-01-08 23:00:04 +0000

    Make circular buffer generic

--- framework/include/framework/buffer/circular.hpp
+++ framework/include/framework/buffer/circular.hpp
@@ -1,34 +1,131 @@
 #ifndef CIRCULAR_HPP
 #define CIRCULAR_HPP
 
+#include <cstddef>
+
 #include "framework/fwcommon.hpp"
 #include "framework/memory/cache_ptr.hpp"
-#include <mutex>
+
 namespace strangeio {
 namespace buffer {
 
+template<typename T>
 class circular {
 public:
-	circular();
-	virtual ~circular();
+	typedef T		value_type;
+	typedef size_t	size_type;
+	
+public:
+	circular()
+		: m_start(0), m_end(0)
+		, m_size(0), m_capacity(0)
+		, m_buffer(nullptr)
+		{ };
 
-	void set_size(int size);
-	int size() const;
-        
-        int load() const;
+	virtual ~circular() { 
+		if(m_buffer) delete[] m_buffer; 
+	};
+
+	void reserve(size_type new_cap) noexcept {
+
+		if(new_cap <= m_capacity) return;
+
+		auto t = new T[new_cap];
+		
+		if(m_size)
+			std::copy(m_buffer, m_buffer+m_size, t);
+		
+		m_buffer = t;
+		m_capacity = new_cap;
+	}
+
+	size_type capacity() const noexcept {
+		return m_capacity;
+	};
 	
-	bool insert(PcmSample* samples, unsigned int num);
+	size_type size() const {
+		return m_size;
+	}
 	
-	bool get(PcmSample* out, unsigned int num);
+    void push_back(value_type& item) {
+		m_buffer[increment()] = item;
+	}
+	
+	void push_back(value_type&& item) {
+		m_buffer[increment()] = item;
+	}
+	
+	void push_many(value_type* items, size_t count) {
+		for(auto i = 0u; i < count; i++) {
+			m_buffer[increment()] = items[i];
+		}
+	}
+	
+	value_type& front() const noexcept {
+		return m_buffer[m_start];
+	}
+	
+	value_type&& move_front() noexcept {
+		return std::move(m_buffer[decrement()]);
+	}
+	
+	void pop_front() {
+		if(!m_size) return;
+		decrement();
+	}
+	
+	void pop_many(value_type* dest, size_type count) {
+		if(!m_size) return;
+		
+		for(auto i = 0u; i < count; i++) {
+			dest[i] = m_buffer[decrement()];
+		}
+	}
+	
+	void move_many(value_type* dest, size_type count) {
+		if(!m_size) return;
+
+		for(auto i = 0u; i < count; i++) {
+			dest[i] = std::move(m_buffer[decrement()]);
+		}
+	}
+
+	value_type& at(size_type pos) const {
+		if(pos > m_size) throw std::out_of_range("Position out of range [siobuf::ciruclar]");
+		return m_buffer[pos];
+	}
+	
+	bool empty() const noexcept {
+		return m_size == 0;
+	}
+	
+	void clear() noexcept {
+		m_size = m_start = m_end = 0;
+	}
+	
 
 private:
-    std::mutex m_mutex;
-    unsigned int m_size, m_start, m_end;
-    signed int m_load;
+	unsigned int m_start, m_end;
+	size_type m_size, m_capacity;
 	
-    PcmSample* m_buffer, *m_ptr;
+    value_type* m_buffer;
+	
+	size_type increment() {
+		auto n = m_end++;
+		m_end = m_end % m_capacity;
+		if(++m_size > m_capacity) m_size = m_capacity;
+		return n;
+	}
+
+	size_type decrement() {
+		auto n = m_start++;
+		m_start = m_start % m_capacity;
+		m_size--;
+		return n;
+	}
 };
 
+template class circular<PcmSample>;
 }
 }
 #endif /* CIRCULAR_HPP */
--- test/catch_buffer.cpp
+++ test/catch_buffer.cpp
@@ -10,12 +10,122 @@
 
 #define IN_SIZE 518
 #define OUT_SIZE 512
-#define LOOP_NUM 1000
+#define LOOP_NUM 500
+
+TEST_CASE( "Circular Buffer capacity",
+"[strangeio::buffer],[strangeio::buffer::circular]\
+[strangeio::buffer::circular::capacity][strangeio::buffer::circular::basic]" ) {
+	strangeio::buffer::circular<PcmSample> cb;
+	
+	REQUIRE(cb.capacity() == 0);
+	cb.reserve(3);
+	REQUIRE(cb.capacity() == 3);
+}
+
+TEST_CASE( "Circular Buffer push",
+"[strangeio::buffer],[strangeio::buffer::circular]\
+[strangeio::buffer::circular::push][strangeio::buffer::circular::basic]" ) {
+	strangeio::buffer::circular<PcmSample> cb;
+	cb.reserve(3);
+	PcmSample s = 0.6f;
+	PcmSample m[3] = {0.5f, 0.6f, 0.7f};
+
+	SECTION("rvalue") {
+		cb.push_back(0.5f);
+		REQUIRE(cb.size() == 1);
+	}
+	
+	SECTION("lvalue copy") {
+		
+		cb.push_back(s);
+		REQUIRE(cb.front() == 0.6f);
+		REQUIRE(cb.size() == 1);
+		s = 0.7f;
+		REQUIRE(cb.front() == 0.6f);
+	}
+	
+	SECTION("lvalue move") {
+		cb.push_back(std::move(s));
+		REQUIRE(cb.size() == 1);
+	}
+	
+	SECTION("push many") {
+		cb.push_many(m, 3);
+		REQUIRE(cb.size() == 3);
+	}
+}
+
+TEST_CASE( "Circular Buffer front",
+"[strangeio::buffer],[strangeio::buffer::circular]\
+[strangeio::buffer::circular::front][strangeio::buffer::circular::basic]" ) {
+	strangeio::buffer::circular<PcmSample> cb;
+	cb.reserve(3);
+	cb.push_back(0.5f);
+	REQUIRE(cb.front() == 0.5);
+	REQUIRE(cb.size() == 1);
+}
+
+TEST_CASE( "Circular Buffer pop",
+"[strangeio::buffer],[strangeio::buffer::circular]\
+[strangeio::buffer::circular::pop][strangeio::buffer::circular::basic]" ) {
+	strangeio::buffer::circular<PcmSample> cb;
+	cb.reserve(3);
+	PcmSample s = 0.6f;
+	PcmSample m[3] = {0.5f, 0.6f, 0.7f};
+	PcmSample out[2] = {0};
+	cb.push_many(m, 3);
+	
+	SECTION("pop_back") {
+		cb.pop_front();
+		REQUIRE(cb.size() == 2);
+	}
+
+	SECTION("pop_many") {
+		cb.pop_many(out, 2);
+		REQUIRE(cb.size() == 1);
+		REQUIRE(out[0] == 0.5f);
+		REQUIRE(out[1] == 0.6f);
+	}	
+	
+}
+
+TEST_CASE( "Circular Buffer clear",
+"[strangeio::buffer],[strangeio::buffer::circular]\
+[strangeio::buffer::circular::clear][strangeio::buffer::circular::basic]" ) {
+	strangeio::buffer::circular<PcmSample> cb;
+	cb.reserve(3);
+	cb.push_back(0.08f);
+	cb.clear();
+	REQUIRE(cb.size() == 0);
+}
+
+TEST_CASE( "Circular Overwrite Full Buffer",
+"[strangeio::buffer],[strangeio::buffer::circular]\
+[strangeio::buffer::circular::overwrite][strangeio::buffer::circular::basic]" ) {
+	strangeio::buffer::circular<PcmSample> cb;
+	cb.reserve(3);
+	PcmSample a[3] = {0.5f, 0.3f, 0.1f};
+	PcmSample b[4] = {0.5f, 0.3f, 0.1f, 0.9f};
+	
+	cb.push_many(a, 3);
+	
+	SECTION("Single Overwrite") {
+		cb.push_back(0.7f);
+		REQUIRE(cb.size() == 3);
+		REQUIRE(cb.front() == 0.7f);
+	}
+	
+	SECTION("Many overwrite") {
+		cb.push_many(b, 4);
+		REQUIRE(cb.size() == 3);
+		REQUIRE(cb.front() == 0.9f);		
+	}
+}
 
 TEST_CASE( "Test Circular buffer", "[strangeio::buffer],[strangeio::buffer::circular]" ) {
 
-strangeio::buffer::circular cb;
-cb.set_size(3*(IN_SIZE+4));
+strangeio::buffer::circular<PcmSample> cb;
+cb.reserve(3*(IN_SIZE+4));
 
 auto iv = 1;
 auto ov = 1;
@@ -25,14 +135,14 @@ PcmSample *buf_out = new PcmSample[OUT_SIZE];
 
 SECTION("Linear") {
 	for( auto i = 0u; i < LOOP_NUM; i++) {
-		if(cb.load() < OUT_SIZE) {
+		if(cb.size() < OUT_SIZE) {
 			for(auto j = 0; j < IN_SIZE; j++) {
 				buf_in[j] = iv++;
 			}
-			cb.insert(buf_in, IN_SIZE);
+			cb.push_many(buf_in, IN_SIZE);
 		}
 
-		cb.get(buf_out, OUT_SIZE);
+		cb.pop_many(buf_out, OUT_SIZE);
 		for(auto k = 0u; k < OUT_SIZE; k++) {
 			REQUIRE(buf_out[k] == ov++);
 		}
@@ -44,7 +154,7 @@ SECTION("De/Interleaved") {
 	auto ivo = 1;
 	for( auto i = 0u; i < LOOP_NUM; i++) {
 		
-		if(cb.load() < OUT_SIZE) {
+		if(cb.size() < OUT_SIZE) {
 
 			auto frames = IN_SIZE/2;
 			
@@ -61,11 +171,11 @@ SECTION("De/Interleaved") {
 				ivo++;
 			}
 
-			cb.insert(buf_work, IN_SIZE);
+			cb.push_many(buf_work, IN_SIZE);
 		}
 
 		auto frames_out = OUT_SIZE/2;
-		cb.get(buf_out, OUT_SIZE);
+		cb.pop_many(buf_out, OUT_SIZE);
 	
 		auto ot = ov;
 		for(auto j = 0; j < OUT_SIZE; j++) {
@@ -82,56 +192,6 @@ SECTION("De/Interleaved") {
 			ov++;
 		}
 	}
-}
-
-
-SECTION("Threaded De/Interleave") {
-	std::thread th([&cb, &iv, &ov, &buf_in, &buf_out]() {
-		PcmSample *buf_work = new PcmSample[IN_SIZE];
-		auto ivo = 1;
-		for( auto i = 0u; i < LOOP_NUM; i++) {
-
-			if(cb.load() < OUT_SIZE) {
-
-				auto frames = IN_SIZE/2;
-
-				for(auto j = 0; j < frames; j++) {
-					buf_in[j] = iv;
-					buf_in[j+frames] = iv*-1;
-					iv++;
-				}
-				strangeio::routine::sound::interleave2(buf_in, buf_work, frames);
-
-				for(auto j = 0; j < IN_SIZE; j++) {
-					REQUIRE(buf_work[j++] == ivo);
-					REQUIRE(buf_work[j] == ivo*-1);
-					ivo++;
-				}
-
-				cb.insert(buf_work, IN_SIZE);
-			}
-
-			auto frames_out = OUT_SIZE/2;
-			cb.get(buf_out, OUT_SIZE);
-
-			auto ot = ov;
-			for(auto j = 0; j < OUT_SIZE; j++) {
-				REQUIRE(buf_out[j++] == ot);
-				REQUIRE(buf_out[j] == (ot*-1));
-				ot++;
-			}
-
-			strangeio::routine::sound::deinterleave2(buf_out, buf_work, frames_out);
-
-			for(auto j = 0; j < frames_out; j++) {
-				REQUIRE(buf_work[j] == ov);
-				REQUIRE(buf_work[j+frames_out] == ov*-1);
-				ov++;
-			}
-		}
-	});
-	
-	th.join();
 }
 
 }

509c063dd8bc88ac59716624d9d69348d2fd72a2
Charlie Fyvie-Gauld <cfg@zunautica.org>
2016-01-10 11:01:36 +0000

    Add a scoped timer to the debug routines
    This can be created at the beginning of the code that needs to be timed
    and when the object drops out of scope and is destroyed, it either
    sets the time delta in an integer or prints a message with the delta

--- framework/include/framework/routine/debug.hpp
+++ framework/include/framework/routine/debug.hpp
@@ -27,15 +27,46 @@ namespace debug {
 		return std::chrono::duration_cast<ms>(end-start).count();
 	}
         
-        inline std::chrono::steady_clock::time_point zero_timepoint() {
-            std::chrono::time_point<steady, std::chrono::duration<int>> tpt(std::chrono::duration<int>(0));
-            return std::chrono::steady_clock::time_point(tpt);
-        }
-        
-        inline auto epoch_ns() {
-            return steady::now().time_since_epoch().count();
-        }
-        
+	inline std::chrono::steady_clock::time_point zero_timepoint() {
+		std::chrono::time_point<steady, std::chrono::duration<int>> tpt(std::chrono::duration<int>(0));
+		return std::chrono::steady_clock::time_point(tpt);
+	}
+
+	inline auto epoch_ns() {
+		return steady::now().time_since_epoch().count();
+	}
+	
+	class scoped_timer {
+	public:
+		scoped_timer(std::string message) 
+			: m_s(clock_time())
+			, m_out(nullptr)
+			, m_msg(message)
+		{ }
+
+		scoped_timer(int* out) 
+			: m_s(clock_time())
+			, m_out(out)
+			, m_msg()
+		{ }
+
+		~scoped_timer() {
+			auto d = clock_delta_us(m_s, clock_time());
+			if(m_out) {
+				*m_out = d;
+			} else	{
+				std::cout 
+					<< "( Profile " << d << "us ) " 
+					<< m_msg << std::endl;
+			}
+		}
+
+	private:
+		tp m_s;
+		int* m_out;
+		std::string m_msg;
+	};
+
 
 }
 }
